a297b5b23a455a9db6142802ab591b6ae9b492c5
shilpa22011999 <shilpameshram342@gmail.com>
2024-07-08 12:55:13 +0530

    ReligionMaster/CountryMaster

---
+++ Web Apps/HIMS.API/Controllers/Masters/Personal Information/CountryMasterController.cs
@@ -0,0 +1,105 @@
+﻿using Asp.Versioning;
+using HIMS.Api.Controllers;
+using HIMS.API.Extensions;
+using HIMS.Core.Domain.Grid;
+using HIMS.Core;
+using HIMS.Data.Models;
+using HIMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using HIMS.Api.Models.Common;
+using HIMS.API.Models.Masters;
+
+namespace HIMS.API.Controllers.Masters.Personal_Information
+{
+
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class CountryMasterController : BaseController
+    {
+        private readonly IGenericService<MCountryMaster> _repository;
+        public CountryMasterController(IGenericService<MCountryMaster> repository)
+        {
+            _repository = repository;
+        }
+
+        //List API
+        [HttpPost]
+        [Route("[action]")]
+        //[Permission(PageCode = "CountryMaster", Permission = PagePermission.View)]
+        public async Task<IActionResult> List(GridRequestModel objGrid)
+        {
+            IPagedList<MCountryMaster> MCountryMasterList = await _repository.GetAllPagedAsync(objGrid);
+            return Ok(MCountryMasterList.ToGridResponse(objGrid, "Country  List"));
+        }
+
+        //List API Get By Id
+        [HttpGet("{id?}")]
+        //[Permission(PageCode = "CountryMaster", Permission = PagePermission.View)]
+        public async Task<ApiResponse> Get(int id)
+        {
+            if (id == 0)
+            {
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status400BadRequest, "No data found.");
+            }
+            var data = await _repository.GetById(x => x.CountryId == id);
+            return data.ToSingleResponse<MCountryMaster, CountryMasterModel>("CountryType");
+        }
+
+        //Add API
+        [HttpPost]
+        //[Permission(PageCode = "CountryMaster", Permission = PagePermission.Add)]
+        public async Task<ApiResponse> Post(CountryMasterModel obj)
+        {
+            MCountryMaster model = obj.MapTo<MCountryMaster>();
+            //model.IsActive = true;
+            if (obj.CountryId == 0)
+            {
+                //model.CreatedBy = CurrentUserId;
+                //model.CreatedDate = DateTime.Now;
+                await _repository.Add(model, CurrentUserId, CurrentUserName);
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Country added successfully.");
+        }
+        //Edit API
+        [HttpPut("{id:int}")]
+        //[Permission(PageCode = "CountryMaster", Permission = PagePermission.Edit)]
+        public async Task<ApiResponse> Edit(CountryMasterModel obj)
+        {
+            MCountryMaster model = obj.MapTo<MCountryMaster>();
+            //model.IsActive = true;
+            if (obj.CountryId == 0)
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            else
+            {
+                //model.ModifiedBy = CurrentUserId;
+                //model.ModifiedDate = DateTime.Now;
+                await _repository.Update(model, CurrentUserId, CurrentUserName, new string[2] { "CreatedBy", "CreatedDate" });
+            }
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Country updated successfully.");
+        }
+
+        //Delete API
+        [HttpDelete]
+        //[Permission(PageCode = "CountryMaster", Permission = PagePermission.Delete)]
+        public async Task<ApiResponse> delete(int Id)
+        {
+            MCountryMaster model = await _repository.GetById(x => x.CountryId == Id);
+            if ((model?.CountryId ?? 0) > 0)
+            {
+                //model.IsActive = false;
+                //model.ModifiedBy = CurrentUserId;
+                //model.ModifiedDate = DateTime.Now;
+                await _repository.SoftDelete(model, CurrentUserId, CurrentUserName);
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Country deleted successfully.");
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+        }
+
+
+
+    }
+}
---
+++ Web Apps/HIMS.API/Models/Masters/ReligionMasterModel.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace HIMS.API.Models.Masters
+{
+    public class ReligionMasterModel
+    {
+        public long ReligionId { get; set; }
+        public string? ReligionName { get; set; }
+        public bool? IsDeleted { get; set; }
+        public long? AddedBy { get; set; }
+        public long? UpdatedBy { get; set; }
+    }
+
+    public class ReligionMasterModelValidator : AbstractValidator<ReligionMasterModel>
+    {
+        public ReligionMasterModelValidator()
+        {
+            RuleFor(x => x.ReligionName).NotNull().NotEmpty().WithMessage("Religion is required");
+        }
+    }
+}
