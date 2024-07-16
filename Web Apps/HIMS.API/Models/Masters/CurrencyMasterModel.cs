282a9e510e7cc668e19c6cb91edc5ae25a7e0276
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-07-17 01:51:18 +0530

    Master16 jul

---
+++ Web Apps/HIMS.API/Models/Masters/CurrencyMasterModel.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace HIMS.API.Models.Masters
+{
+    public class CurrencyMasterModel
+    {
+        public long CurrencyId { get; set; }
+        public string? CurrencyName { get; set; }
+
+    }
+    public class CurrencyMasterModelValidator : AbstractValidator<CurrencyMasterModel>
+    {
+        public CurrencyMasterModelValidator()
+        {
+            RuleFor(x => x.CurrencyName).NotNull().NotEmpty().WithMessage("Patient Type is required");
+        }
+    }
+}
