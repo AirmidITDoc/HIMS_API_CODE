07408a5da82008e8436116f4ed835bda3288bc6a
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-07-22 19:23:17 +0530

    InstructionMaster

---
+++ Web Apps/HIMS.API/Models/Masters/InstructionMasterModel.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace HIMS.API.Models.Masters
+{
+    public class InstructionMasterModel
+    {
+        public long InstructionId { get; set; }
+        public string? InstructionDescription { get; set; }
+    }
+    public class InstructionMasterModelValidator : AbstractValidator<InstructionMasterModel>
+    {
+        public InstructionMasterModelValidator()
+        {
+            RuleFor(x => x.InstructionDescription).NotNull().NotEmpty().WithMessage("Instruction  is required");
+        }
+    }
+}
