c87ac8382130878c3b8644d72c8931b8a0b435f6
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-08-08 19:53:50 +0530

    ItemMaster and Supplier master

---
+++ Libraries/HIMS.Services/Pathology/IPathologyReportTemplateService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HIMS.Data.Models;
+
+namespace HIMS.Services.Pathology
+{
+    public partial interface IPathologyReportTemplateService
+    {
+        //Task InsertAsyncSP(TPathologyReportTemplateDetails objpathology, int PathReportId, int PathTemplateId);
+    }
+}
---
+++ Web Apps/HIMS.API/Models/Pathology/PathologyReportTemplateModel.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace HIMS.API.Models.Pathology
+{
+    public class PathologyReportTemplateModel
+    {
+        public long PathReportTemplateDetId { get; set; }
+        public long? PathReportId { get; set; }
+        public long? PathTemplateId { get; set; }
+        public string? PathTemplateDetailsResult { get; set; }
+        public long? TestId { get; set; }
+      
+    }
+    public class PathologyReportTemplateModelValidator : AbstractValidator<PathologyReportTemplateModel>
+    {
+        public PathologyReportTemplateModelValidator()
+        {
+            RuleFor(x => x.PathReportTemplateDetId).NotNull().NotEmpty().WithMessage("PathReportTemplateDetId is required");
+            RuleFor(x => x.PathReportId).NotNull().NotEmpty().WithMessage("PathReportId is required");
+            RuleFor(x => x.PathTemplateId).NotNull().NotEmpty().WithMessage("From StoreId is required");
+            RuleFor(x => x.PathTemplateDetailsResult).NotNull().NotEmpty().WithMessage(" PathTemplateId is required");
+            RuleFor(x => x.TestId).NotNull().NotEmpty().WithMessage(" TestId is required");
+        }
+    }
+}
