045accd4f8d2c382be726f41e43f7ad96e0f1e51
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-08-08 10:24:52 +0530

    Application mapping

--- Web Apps/HIMS.API/Infrastructure/ApplicationMappingProfile.cs
+++ Web Apps/HIMS.API/Infrastructure/ApplicationMappingProfile.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using HIMS.API.Controllers.Masters.Personal_Information;
-using HIMS.API.Models;
 using HIMS.API.Models.Inventory;
 using HIMS.API.Models.Masters;
 using HIMS.API.Models.OutPatient;
@@ -39,7 +38,7 @@ namespace HIMS.API.Infrastructure
 
            
             CreateMap<MSupplierMaster, SupplierModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
-            CreateMap<MTalukaMaster, TalukaMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            
             CreateMap<MAreaMaster, AreaMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             
             CreateMap<CompanyMaster, CompanyMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
@@ -124,7 +123,7 @@ namespace HIMS.API.Infrastructure
             CreateMap<TIndentDetail, IndentDetailModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<TIndentHeader, IndentVerifyModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
            
-            CreateMap<MTalukaMaster, TalukaMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            
             CreateMap<MAreaMaster, AreaMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<MBankMaster, BankMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<CompanyMaster, CompanyMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
