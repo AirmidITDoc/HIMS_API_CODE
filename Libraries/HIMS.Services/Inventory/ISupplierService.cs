1ef4e9231cba26afc076d5d0fca3e62d5938c0e5
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-08-08 09:59:41 +0530

    Supllier Master Service and Interface file deleted

---
+++ Libraries/HIMS.Services/Inventory/ISupplierService.cs
@@ -0,0 +1,15 @@
+﻿using HIMS.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Services.Inventory
+{
+    public partial interface ISupplierService
+    {
+        Task InsertAsyncSP(MSupplierMaster objSupplier, MAssignSupplierToStore objAssignSupplier, int currentUserId, string currentUserName);
+
+    }
+}
---
+++ Web Apps/HIMS.API/Controllers/Inventory/SupplierMasterController.cs
@@ -0,0 +1,45 @@
+﻿using HIMS.Api.Controllers;
+using HIMS.Api.Models.Common;
+using HIMS.API.Extensions;
+using HIMS.API.Models.OutPatient;
+using HIMS.Data.Models;
+using HIMS.Services.OutPatient;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HIMS.API.Controllers.Inventory
+{
+    public class SupplierMasterController : BaseController
+    {
+        private readonly IAppointmentService _IAppointmentService;
+        public SupplierMasterController(IAppointmentService repository)
+        {
+            _IAppointmentService = repository;
+        }
+
+        [HttpPost("AppointmentInsert")]
+        //[Permission(PageCode = "Sales", Permission = PagePermission.Add)]
+        public async Task<ApiResponse> Insert(AppointmentReqDto obj)
+        {
+            MSupplierMaster model = obj.Registration.MapTo<MSupplierMaster>();
+            MAssignSupplierToStore objAssignSupplier = obj.Visit.MapTo<MAssignSupplierToStore>();
+            if (obj.Registration.RegID == 0)
+            {
+                //model.RegDate = Convert.ToDateTime(obj.Registration.RegDate);
+                //model.RegTime = Convert.ToDateTime(obj.Registration.RegTime);
+                model.AddedBy = CurrentUserId;
+
+                if (obj.Visit.VisitId == 0)
+                {
+                    //objVisitDetail.VisitDate = Convert.ToDateTime(obj.Visit.VisitDate);
+                    //objVisitDetail.VisitTime = Convert.ToDateTime(obj.Visit.VisitTime);
+                    //objVisitDetail.AddedBy = CurrentUserId;
+                    //objVisitDetail.UpdatedBy = CurrentUserId;
+                }
+                //await _IAppointmentService.InsertAsyncSP(model, objVisitDetail, CurrentUserId, CurrentUserName);
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Supplier Name  added successfully.");
+        }
+    }
+}
