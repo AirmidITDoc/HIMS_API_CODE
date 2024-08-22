b369b95e122701d76524af6040a55e6afa00b9fb
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-08-22 23:38:34 +0530

    TestMaster

--- Web Apps/HIMS.API/Models/Masters/TestMasterModel.cs
+++ Web Apps/HIMS.API/Models/Masters/TestMasterModel.cs
@@ -8,28 +8,32 @@ namespace HIMS.API.Models.Masters
 
         public long TestId { get; set; }
         public string? TestName { get; set; }
-         public string? PrintTestName { get; set; }
+        public string? PrintTestName { get; set; }
+        public long? CategoryId { get; set; }
+        public bool? IsSubTest { get; set; }
+        public string? TechniqueName { get; set; }
+        public string? MachineName { get; set; }
+        public string? SuggestionNote { get; set; }
+        public string? FootNote { get; set; }
         public bool? IsActive { get; set; }
         public long? AddedBy { get; set; }
-        public string? CreatedDate { get; set; }
-        public int? CreatedBy { get; set; }
-        public string? TestTime { get; set; }
+        public long? UpdatedBy { get; set; }
+        public long? ServiceId { get; set; }
         public long? IsTemplateTest { get; set; }
-
-
-
-        public List<PathTemplateDetailModel> MPathTemplateDetail { get; set; }
+        public string? CreatedDate { get; set; }
+        public string? TestTime { get; set; }
+         public List<PathTemplateDetailModel> MPathTemplateDetail { get; set; }
         
          }
-
-    public class TestMasterModelValidator : AbstractValidator<TestMasterModel>
-    {
+        public class TestMasterModelValidator : AbstractValidator<TestMasterModel>
+         {
         public TestMasterModelValidator()
         {
             RuleFor(x => x.TestName).NotNull().NotEmpty().WithMessage("TestName  is required");
-
             RuleFor(x => x.PrintTestName).NotNull().NotEmpty().WithMessage("PrintTestName  is required");
-
+            RuleFor(x => x.CategoryId).NotNull().NotEmpty().WithMessage("CategoryId  is required");
+            RuleFor(x => x.IsSubTest).NotNull().NotEmpty().WithMessage("IsSubTest  is required");
+            RuleFor(x => x.ServiceId).NotNull().NotEmpty().WithMessage("ServiceId  is required");
 
 
         }
@@ -46,15 +50,10 @@ namespace HIMS.API.Models.Masters
     {
         public PathTemplateDetailModelValidator()
         {
-            RuleFor(x => x.PtemplateId).NotNull().NotEmpty().WithMessage("PtemplateId is required");
+           
             RuleFor(x => x.TestId).NotNull().NotEmpty().WithMessage("TestId is required");
             RuleFor(x => x.TemplateId).NotNull().NotEmpty().WithMessage("TemplateId is required");
-
-
-
         }
-
-       
 
     }
 }
