0f29363e70d48646b591da829d8a8a8b43f363d6
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-09-05 10:47:46 +0530

    SupplierMaster

---
+++ Libraries/HIMS.Services/Inventory/ISupplierService.cs
@@ -0,0 +1,24 @@
+﻿using HIMS.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Services.Inventory
+{
+    public partial interface ISupplierService
+    {
+
+        //Task InsertAsync(MSupplierMaster objSupplier, List<MAssignSupplierToStore> objStore, int UserId, string Username);
+
+        Task InsertAsync(MSupplierMaster objSupplier, List<MAssignSupplierToStore> newSupplierStore, int UserId, string Username);
+
+
+
+
+        Task UpdateAsync(MSupplierMaster objSupplier, int UserId, string Username);
+
+
+    }
+}
