72668cc18bca1b22601850a74294de0d35d7aa09
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-11-07 22:15:55 +0530

    list

---
+++ Libraries/HIMS.Data/DTO/OPPatient/OPRegistrationList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.OPPatient
+{
+    public class OPRegistrationList
+    {
+        //public int RegId { get; set; }
+
+        //public DateTime RegTime { get; set; }
+        // public long RegNo { get; set; }
+        public string PatientName { get; set; }
+       // public string MobileNo { get; set; }
+       // public string Address { get; set; }
+       //public string City { get; set; }
+        //public decimal TotalAmt { get; set; }
+        //public Double ConcessionAmt { get; set; }
+
+        //public decimal NetPayableAmt { get; set; }
+
+        // public int OPD_IPD_ID { get; set; }
+        //public decimal PaidAmt { get; set; }
+
+        //public decimal BalanceAmt { get; set; }
+
+        //public decimal CashPay { get; set; }
+
+        //public decimal ChequePay { get; set; }
+        //public decimal CardPay { get; set; }
+
+        //public decimal AdvUsedPay { get; set; }
+        //public decimal OnlinePay { get; set; }
+
+
+        // public Double PayCount { get; set; }
+    }
+}
+
