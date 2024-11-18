4ab6d8311773d7d5efe5a4e06361c39ed2796d43
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-11-18 16:58:36 +0530

    LabRequest

---
+++ Libraries/HIMS.Data/DTO/IPPatient/LabRequestDetailsListDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.IPPatient
+{
+    public  class LabRequestDetailsListDto
+    {
+        public long ReqDetId { get; set; }
+        public long? RequestId { get; set; }
+        public long? ServiceId { get; set; }
+        public decimal? Price { get; set; }
+        public bool? IsStatus { get; set; }
+        public long? AddedBillingId { get; set; }
+        public DateTime? AddedByDate { get; set; }
+        public DateTime? AddedByTime { get; set; }
+        public long? CharId { get; set; }
+        public bool? IsTestCompted { get; set; }
+        public bool? IsOnFileTest { get; set; }
+    }
+}
--- Libraries/HIMS.Data/DTO/IPPatient/PrescriptionReturnDto.cs
+++ Libraries/HIMS.Data/DTO/IPPatient/PrescriptionReturnDto.cs
@@ -8,6 +8,9 @@ namespace HIMS.Data.DTO.IPPatient
 {
     public class PrescriptionReturnDto
     {
-        public long PresReId {  get; set; } 
+        public long PresReId { get; set; }
+        public string? ItemName { get; set; }
+        public string? BatchNo { get; set; }
+        public double? Qty { get; set; }
     }
 }
