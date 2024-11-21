9af02a26c73197a0f9019b8d9f474cd4b002b661
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-11-21 10:51:05 +0530

    GenericMaster

--- Web Apps/HIMS.API/Controllers/Masters/Personal Information/GenericMasterController.cs
+++ Web Apps/HIMS.API/Controllers/Masters/Personal Information/GenericMasterController.cs
@@ -52,13 +52,13 @@ namespace HIMS.API.Controllers.Masters.Personal_Information
             model.IsActive = true;
             if (obj.GenericId == 0)
             {
-                //model.CreatedBy = CurrentUserId;
-                //model.CreatedDate = DateTime.Now;
+                model.CreatedBy = CurrentUserId;
+                model.CreatedDate = DateTime.Now;
                 await _repository.Add(model, CurrentUserId, CurrentUserName);
             }
             else
                 return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
-            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Generic name added successfully.");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "GenericName added successfully.");
         }
         //Edit API
         [HttpPut("{id:int}")]
@@ -75,7 +75,7 @@ namespace HIMS.API.Controllers.Masters.Personal_Information
                 model.ModifiedDate = DateTime.Now;
                 await _repository.Update(model, CurrentUserId, CurrentUserName, new string[2] { "CreatedBy", "CreatedDate" });
             }
-            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Generic Name updated successfully.");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "GenericName  updated successfully.");
         }
         //Delete API
         [HttpDelete]
@@ -89,7 +89,7 @@ namespace HIMS.API.Controllers.Masters.Personal_Information
                 model.ModifiedBy = CurrentUserId;
                 model.ModifiedDate = DateTime.Now;
                 await _repository.SoftDelete(model, CurrentUserId, CurrentUserName);
-                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Generic name deleted successfully.");
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "GenericName  deleted successfully.");
             }
             else
                 return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
