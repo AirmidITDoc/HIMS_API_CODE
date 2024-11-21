924c655fe675f6f72196ff696fe2e9c3434bc354
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-11-21 17:19:16 +0530

    LabRequestDetails

--- Libraries/HIMS.Data/DTO/IPPatient/LabRequestDetailsListDto.cs
+++ Libraries/HIMS.Data/DTO/IPPatient/LabRequestDetailsListDto.cs
@@ -8,29 +8,25 @@ namespace HIMS.Data.DTO.IPPatient
 {
     public  class LabRequestDetailsListDto
     {
-        public long RequestId { get; set; }           
-        //public long? ReqDetId { get; set; }           
-        //public string? ServiceName { get; set; }      
-        //public decimal? ServiceId { get; set; }        
-        //public bool? OP_IP_ID { get; set; }           
-        //public long? OP_IP_Type { get; set; }         
-        //public bool? IsStatus { get; set; }           
-        //public DateTime? ReqDate { get; set; }        
-        //public TimeSpan? ReqTime { get; set; }        
-        //public string? AddedByName { get; set; }      
-        //public string? BillingUser { get; set; }      
-        //public DateTime? AddedByDate { get; set; }    
-        //public bool? IsPathology { get; set; }        
-        //public bool? IsRadiology { get; set; }        
-        //public long? CharId { get; set; }             
-        //public bool? IsTestCompleted { get; set; }    
-        //public long? PathReportID { get; set; }       
-        //public bool? IsTemplateTest { get; set; }     
-        //public long? PBillNo { get; set; }            
-        //public long? ChargeId { get; set; }          
-
-
-
-
+        public long RequestId { get; set; }
+        public long? ReqDetId { get; set; }
+        public string? ServiceName { get; set; }
+        public long? ServiceId { get; set; }
+        public long OP_IP_ID { get; set; }
+        public long OP_IP_Type { get; set; }
+        public bool? IsStatus { get; set; }
+        public string ReqDate { get; set; }
+        public string ReqTime { get; set; }
+        public string? AddedByName { get; set; }
+        public string? BillingUser { get; set; }
+        public string? AddedByDate { get; set; }
+        public long IsPathology { get; set; }
+        public long IsRadiology { get; set; }        
+        public long CharId { get; set; }             
+        public long? IsTestCompleted { get; set; }
+        public long? PathReportID { get; set; }
+        public long? IsTemplateTest { get; set; }
+        public  string PBillNo { get; set; }
+        public long? ChargeId { get; set; }
     }
 }
