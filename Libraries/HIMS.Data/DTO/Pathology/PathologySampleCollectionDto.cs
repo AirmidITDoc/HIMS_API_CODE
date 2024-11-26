fbcc8cf73a47f85ab69c75d9878fc0e6d8df02d9
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-11-26 10:40:16 +0530

    PathologysampleCollection/PatientList

--- Libraries/HIMS.Data/DTO/Pathology/PathologySampleCollectionDto.cs
+++ Libraries/HIMS.Data/DTO/Pathology/PathologySampleCollectionDto.cs
@@ -6,22 +6,26 @@ using System.Threading.Tasks;
 
 namespace HIMS.Data.DTO.Pathology
 {
-    public  class PathologySampleCollectionDto
+    public class PathologySampleCollectionDto
     {
         public string RegNo { get; set; }
         public string? PatientName { get; set; }
         //public long? OP_IP_No { get; set; }
-        //public DateTime? VADate { get; set; }
-        //public DateTime? DOA { get; set; }         
-        //public DateTime? DOT { get; set; }           
-        //public string? DoctorName { get; set; }      
-        //public long? Visit_Adm_ID { get; set; }      
-        //public long? PatientType { get; set; }      
-        //public long? PBillNo { get; set; }           
-        //public long? IsSampleCollection { get; set; } 
-        //public string? BillNo { get; set; }          
-        //public DateTime? PathDate { get; set; }      
-        //public string? WardName { get; set; }        
+        //public string VADate { get; set; }
+        //public string DOA { get; set; }
+        //public DateTime? DOT { get; set; }
+        //public string DoctorName { get; set; }
+        //public long? Visit_Adm_ID { get; set; }
+        //public string? LBL { get; set; }
+        //public int PBillNo { get; set; }
+        //public long? IsSampleCollection { get; set; }
+        //public string? BillNo { get; set; }
+        //public DateTime? PathDate { get; set; }
+        //public string? WardName { get; set; }
+        //public string? CompanyName { get; set; }
+        //public string? PatientType { get; set; }
+
+
 
 
     }
