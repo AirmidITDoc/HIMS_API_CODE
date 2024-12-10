57ad712b9a2e508d3331a99a46449c58687e0d40
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2024-12-10 18:26:00 +0530

    storeMaster/classMaster

--- Web Apps/HIMS.API/Models/Inventory/Masters/StoreMasterModel.cs
+++ Web Apps/HIMS.API/Models/Inventory/Masters/StoreMasterModel.cs
@@ -7,6 +7,39 @@ namespace HIMS.API.Models.Masters
         public long StoreId { get; set; }
         public string? StoreShortName { get; set; }
         public string? StoreName { get; set; }
+        public string? IndentPrefix { get; set; }
+        public string? IndentNo { get; set; }
+        public string? PurchasePrefix { get; set; }
+        public string? PurchaseNo { get; set; }
+        public string? GrnPrefix { get; set; }
+        public string? GrnNo { get; set; }
+        public string? GrnreturnNoPrefix { get; set; }
+        public string? GrnreturnNo { get; set; }
+        public string? IssueToDeptPrefix { get; set; }
+        public string? IssueToDeptNo { get; set; }
+        public string? ReturnFromDeptNoPrefix { get; set; }
+        public string? ReturnFromDeptNo { get; set; }
+        public string? WorkOrderPrefix { get; set; }
+        public string? WorkOrderNo { get; set; }
+        public long? PharSalCountId { get; set; }
+        public long? PharSalRecCountId { get; set; }
+        public long? PharSalReturnCountId { get; set; }
+        public long? PharAdvId { get; set; }
+        public long? PharAdvReptId { get; set; }
+        public long? PharAdvRefId { get; set; }
+        public long? PharAdvRefReptId { get; set; }
+        public string? PrintStoreName { get; set; }
+        public string? DlNo { get; set; }
+        public string? Gstin { get; set; }
+        public string? StoreAddress { get; set; }
+        public string? HospitalMobileNo { get; set; }
+        public string? HospitalEmailId { get; set; }
+        public string? PrintStoreUnitName { get; set; }
+        public bool? IsPharStore { get; set; }
+        public bool? IsWhatsAppMsg { get; set; }
+        public string? WhatsAppTemplateId { get; set; }
+        public bool? IsSmsmsg { get; set; }
+        public string? SmstemplateId { get; set; }
     }
 
     public class StoreMasterModelValidator : AbstractValidator<StoreMasterModel>
@@ -15,6 +48,8 @@ namespace HIMS.API.Models.Masters
         {
             RuleFor(x => x.StoreName).NotNull().NotEmpty().WithMessage(" Store Name is required");
             RuleFor(x => x.StoreShortName).NotNull().NotEmpty().WithMessage(" StoreShortName  is required");
+            RuleFor(x => x.IndentPrefix).NotNull().NotEmpty().WithMessage(" IndentPrefix  is required");
+
 
         }
     }
