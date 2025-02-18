15141db6096bc637bafd7f53e87f64c071c20abb
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-02-18 12:55:30 +0530

    Menu Master List ,Insert and Update completd

--- Libraries/HIMS.Services/Masters/IMenuMasterService.cs
+++ Libraries/HIMS.Services/Masters/IMenuMasterService.cs
@@ -13,7 +13,7 @@ namespace HIMS.Services.Masters
     {
         Task InsertAsyncSP(MenuMaster objMenuMaster, int UserId, string Username);
         Task InsertAsync(MenuMaster objMenuMaster, int UserId, string Username);
-        Task UpdateAsync(MenuMaster objMenuMaster, int UserId, string Username);
+        Task UpdateAsyncSP(MenuMaster objMenuMaster, int UserId, string Username);
         Task<IPagedList<MenuMasterListDto>> MenuMasterList(GridRequestModel objGrid);
 
     }
