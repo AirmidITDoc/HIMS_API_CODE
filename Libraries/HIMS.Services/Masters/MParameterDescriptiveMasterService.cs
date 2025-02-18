5d6c68bd4c7b34ab803645f7dbf21256a598e744
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2025-02-18 21:19:35 +0530

    Paymentcontroller,Parametermaster controller

--- Libraries/HIMS.Services/Masters/MParameterDescriptiveMasterService.cs
+++ Libraries/HIMS.Services/Masters/MParameterDescriptiveMasterService.cs
@@ -11,10 +11,10 @@ namespace HIMS.Services.Masters
 {
     public partial class MParameterDescriptiveMasterService : IMParameterDescriptiveMasterService
     {
-        public virtual async Task<IPagedList<MParameterDescriptiveMasterListDto>> GetListAsync(GridRequestModel model)
-        {
-            return await DatabaseHelper.GetGridDataBySp<MParameterDescriptiveMasterListDto>(model, "m_Rtrv_PathParameterDescriptiveMaster_by_Name");
-        }
+        //public virtual async Task<IPagedList<MParameterDescriptiveMasterListDto>> GetListAsync(GridRequestModel model)
+        //{
+        //    return await DatabaseHelper.GetGridDataBySp<MParameterDescriptiveMasterListDto>(model, "m_Rtrv_PathParameterDescriptiveMaster_by_Name");
+        //}
         public virtual async Task<IPagedList<MParameterDescriptiveMasterListDto>> GetListAsync1(GridRequestModel model)
         {
             return await DatabaseHelper.GetGridDataBySp<MParameterDescriptiveMasterListDto>(model, "m_Rtrv_PathDescriptiveValues_1");
