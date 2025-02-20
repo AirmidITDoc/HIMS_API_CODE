e1bc6960285cac50d431648385e3b618b2a01cbf
Rahul1997rjs <rahuljagannath444@gmail.com>
2025-02-20 22:31:30 +0530

    SubTpaCompany

--- Web Apps/HIMS.API/Models/Inventory/Masters/SubTpaModel.cs
+++ Web Apps/HIMS.API/Models/Inventory/Masters/SubTpaModel.cs
@@ -13,6 +13,16 @@ namespace HIMS.API.Models.Masters
         public string? PhoneNo { get; set; }
         public string? MobileNo { get; set; }
         public string? FaxNo { get; set; }
+        public bool? IsActive { get; set; }
+        public long? AddedBy { get; set; }
+        public long? UpdatedBy { get; set; }
+        public bool? IsCancelled { get; set; }
+        public long? IsCancelledBy { get; set; }
+        public DateTime? IsCancelledDate { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
     }
 
     public class SubTpaModelValidator : AbstractValidator<SubTpaModel>
