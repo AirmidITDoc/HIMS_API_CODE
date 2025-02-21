fe89b07477c82caf84a97519c217ed8561210bd5
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-02-21 12:20:59 +0530

    Role Controller and service file created

---
+++ Libraries/HIMS.Services/Administration/IRoleService.cs
@@ -0,0 +1,18 @@
+﻿using HIMS.Core.Domain.Grid;
+using HIMS.Data.DTO.Administration;
+using HIMS.Data.DTO.Inventory;
+using HIMS.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Services.Administration
+{
+    public partial interface IRoleService    
+    {
+        List<MenuModel> GetPermisison(int RoleId);
+        //void SavePermission(List<PermissionModel> lst);
+    }
+}
---
+++ Web Apps/HIMS.API/Controllers/Administration/RoleController.cs
@@ -0,0 +1,30 @@
+﻿using HIMS.Api.Controllers;
+using HIMS.Services.Administration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HIMS.API.Controllers.Administration
+{
+    public class RoleController : BaseController
+    { 
+        private readonly IRoleService _IroleService;
+        public RoleController(IRoleService repository)
+        {
+            _IroleService = repository;
+        }
+
+
+        [HttpGet]
+        [Route("get-permissions")]
+        public IActionResult GetPermission(int RoleId)
+        {
+            return Ok(_IroleService.GetPermisison(RoleId));
+        }
+        //[HttpPost]
+        //[Route("save-permission")]
+        //public IActionResult PostPermission(List<PermissionModel> obj)
+        //{
+        //    _IroleService.SavePermission(obj);
+        //    return Ok();
+        //}
+    }
+}
