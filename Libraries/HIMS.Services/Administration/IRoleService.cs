f6d634fc48dd3360b9abc1837a62f1d6d8e643c5
Vimal Vataliya <elaunch.vimal@gmail.com>
2025-02-27 21:33:53 +0530

    set permission apis.

--- Libraries/HIMS.Services/Administration/IRoleService.cs
+++ Libraries/HIMS.Services/Administration/IRoleService.cs
@@ -1,6 +1,7 @@
 ﻿using HIMS.Core.Domain.Grid;
 using HIMS.Data.DTO.Administration;
 using HIMS.Data.DTO.Inventory;
+using HIMS.Data.DTO.User;
 using HIMS.Data.Models;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,9 @@ using System.Threading.Tasks;
 
 namespace HIMS.Services.Administration
 {
-    public partial interface IRoleService    
+    public partial interface IRoleService
     {
         List<MenuModel> GetPermisison(int RoleId);
-        //void SavePermission(List<PermissionModel> lst);
+        void SavePermission(List<PermissionModel> lst);
     }
 }
