49a3ebdf8c039b51c08e9bb77661cc4650f1765a
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2025-03-02 11:23:42 +0530

    not fr my side

---
+++ Libraries/HIMS.Data/DTO/IPPatient/IPAddchargesListDto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.IPPatient
+{
+    public class IPAddchargesListDto
+    {
+        public long? ChargesId { get; set; }
+        public long OPD_IPD_Id { get; set; }
+
+        public long? ServiceId { get; set; }
+        public string? ServiceName { get; set; }
+        public double? Price { get; set; }
+        public double? Qty { get; set; }
+        public double? TotalAmt { get; set; }
+        public double? ConcessionPercentage { get; set; }
+
+        public decimal? ConcessionAmount { get; set; }
+        public decimal? NetAmount { get; set; }
+        public long? DoctorId { get; set; }
+        public string? DoctorName { get; set; }
+        public double? DocPercentage { get; set; }
+        public double? DocAmt { get; set; }
+        public double? HospitalAmt { get; set; }
+        public string? ChargesDate { get; set; }
+        public bool? IsGenerated { get; set; }
+        public bool? IsCancelled { get; set; }
+        public long? OPD_IPD_Type { get; set; }
+        public long? IsPathology { get; set; }
+        public long? IsRadiology { get; set; }
+        public long? IsDoctorShareGenerated { get; set; }
+        public byte? IsInterimBillFlag { get; set; }
+        public long? IsPackage { get; set; }
+        public DateTime? CDate { get; set; }
+        public long? PackageId { get; set; }
+        public long? IsPackageMaster { get; set; }
+        public string? ClassName { get; set; }
+        public string? ChargesAddedName { get; set; }
+        public string? ChargesCancelledByName { get; set; }
+        public DateTime? IsCancelledDate { get; set; }
+         public string? C_Price { get; set; }
+         public string? C_qty { get; set; }
+        public string? C_TotalAmount { get; set; }
+        public bool? IsComServ { get; set; }
+        public bool? IsPrintCompSer { get; set; }
+        public string? CompanyServiceName { get; set; }
+        public long? ClassId { get; set; }
+        public string? IsPathTestCompleted { get; set; }
+        public string? IsRadTestCompleted { get; set; }
+
+
+    }
+
+}
---
+++ Web Apps/HIMS.API/Models/OutPatient/PaymentModel.cs
@@ -0,0 +1,103 @@
+﻿using System.Drawing.Printing;
+using FluentValidation;
+using HIMS.API.Models.IPPatient;
+using HIMS.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HIMS.API.Models.OutPatient
+{
+    public class PaymentModel11
+    {
+
+
+        public long PaymentId { get; set; }
+        public long? BillNo { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public string? PaymentTime { get; set; }
+        public decimal? CashPayAmount { get; set; }
+        public decimal? ChequePayAmount { get; set; }
+        public string? ChequeNo { get; set; }
+        public string? BankName { get; set; }
+        public DateTime? ChequeDate { get; set; }
+        public decimal? CardPayAmount { get; set; }
+        public string? CardNo { get; set; }
+        public string? CardBankName { get; set; }
+        public DateTime? CardDate { get; set; }
+        public decimal? AdvanceUsedAmount { get; set; }
+        public long? AdvanceId { get; set; }
+        public long? RefundId { get; set; }
+        public long? TransactionType { get; set; }
+        public string? Remark { get; set; }
+        public long? AddBy { get; set; }
+        public bool? IsCancelled { get; set; }
+        public long? IsCancelledBy { get; set; }
+        public DateTime? IsCancelledDate { get; set; }
+        public int? OPDIPDType { get; set; }
+        public decimal? NeftpayAmount { get; set; }
+        public string? Neftno { get; set; }
+        public string? NeftbankMaster { get; set; }
+        public DateTime? Neftdate { get; set; }
+        public decimal? PayTmamount { get; set; }
+        public string? PayTmtranNo { get; set; }
+        public DateTime? PayTmdate { get; set; }
+        public decimal? TDSAmount { get; set; }
+
+
+
+
+        public class PaymentModel11Validator : AbstractValidator<PaymentModel11>
+        {
+            public PaymentModel11Validator()
+            {
+                RuleFor(x => x.BillNo).NotNull().NotEmpty().WithMessage("BillNo is required");
+            }
+        }
+
+
+        public class BillModel
+        {
+            public long? BillNo { get; set; }
+             public decimal? balanceAmt { get; set; }
+
+        }
+        public class BillModelValidator : AbstractValidator<BillModel>
+        {
+            public BillModelValidator()
+            {
+                RuleFor(x => x.BillNo).NotNull().NotEmpty().WithMessage("BillNo is required");
+            }
+        }
+
+        public class AdvanceDetailsModel
+        {
+            public long? AdvanceDetailID { get; set; }
+            public decimal? UsedAmount { get; set; }
+            public decimal? BalanceAmount { get; set; }
+        }
+        public class AdvanceDetailsModelValidator : AbstractValidator<AdvanceDetailsModel>
+        {
+            public AdvanceDetailsModelValidator()
+            {
+                RuleFor(x => x.UsedAmount).NotNull().NotEmpty().WithMessage("UsedAmount is required");
+                RuleFor(x => x.BalanceAmount).NotNull().NotEmpty().WithMessage("BalanceAmount is required");
+            }
+        }
+        
+        public class AdvanceHeadersModel
+        {
+            public long? AdvanceId { get; set; }
+            public decimal? AdvanceUsedAmount { get; set; }
+            public decimal? BalanceAmount { get; set; }
+
+        }
+        public class ModelPayment
+        {
+            public PaymentModel11 Payment { get; set; }
+            public BillModel Billupdate { get; set; }
+            public List<AdvanceDetailsModel> AdvanceDetailupdate { get; set; }
+            public AdvanceHeadersModel AdvanceHeaderupdate { get; set; }
+
+
+        }
+    }
+}
