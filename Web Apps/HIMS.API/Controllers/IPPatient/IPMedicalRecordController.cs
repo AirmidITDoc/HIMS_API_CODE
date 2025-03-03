26e118ccd8b3230774f8044fbf14924c4b256f6b
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2025-03-03 13:50:27 +0530

    DD medical record

--- Web Apps/HIMS.API/Controllers/IPPatient/IPMedicalRecordController.cs
+++ Web Apps/HIMS.API/Controllers/IPPatient/IPMedicalRecordController.cs
@@ -9,6 +9,7 @@ using HIMS.Services.Inventory;
 using Microsoft.AspNetCore.Mvc;
 using HIMS.Services.IPPatient;
 using HIMS.API.Models.IPPatient;
+using HIMS.Data;
 
 namespace HIMS.API.Controllers.IPPatient
 {
@@ -18,9 +19,20 @@ namespace HIMS.API.Controllers.IPPatient
     public class IPMedicalRecordController : BaseController
     {
         private readonly IMedicalRecordService _MedicalRecordService;
-        public IPMedicalRecordController(IMedicalRecordService repository)
+        private readonly IGenericService<ServiceMaster> _serviceMasterrepository;
+        private readonly IGenericService<MOpcasepaperDignosisMaster> _Dignos;
+        private readonly IGenericService<MExaminationMaster> _Examination;
+        private readonly IGenericService<MComplaintMaster> _MComplaintMaster;
+
+        
+        public IPMedicalRecordController(IMedicalRecordService repository, IGenericService<ServiceMaster> ServiceMasterrepository, IGenericService<MOpcasepaperDignosisMaster> MOpcasepaperDignosisMasterrepository, IGenericService<MExaminationMaster> MExaminationMasterrepository
+            ,IGenericService<MComplaintMaster> MComplaintMasterrepository)
         {
             _MedicalRecordService = repository;
+            _serviceMasterrepository = ServiceMasterrepository;
+            _Dignos = MOpcasepaperDignosisMasterrepository;
+            _Examination = MExaminationMasterrepository;
+            _MComplaintMaster = MComplaintMasterrepository;
         }
         [HttpPost("InsertEDMX")]
         [Permission(PageCode = "MedicalRecords", Permission = PagePermission.Add)]
@@ -53,7 +65,44 @@ namespace HIMS.API.Controllers.IPPatient
             return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "MedicalRecord  updated successfully.");
         }
 
-        
+        //List API
+        [HttpGet]
+        [Route("get-Service")]
+        //[Permission(PageCode = "DepartmentMaster", Permission = PagePermission.View)]
+        public async Task<ApiResponse> GetDropdown()
+        {
+            var MServicetMasterList = await _serviceMasterrepository.GetAll(x => x.IsActive.Value);
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Service dropdown", MServicetMasterList.Select(x => new { x.ServiceName, x.ServiceId }));
+        }
+
+        //List API
+        [HttpGet]
+        [Route("get-Dignosis")]
+        //[Permission(PageCode = "DepartmentMaster", Permission = PagePermission.View)]
+        public async Task<ApiResponse> GetDignosDropdown()
+        {
+            var MDignosMasterList = await _Dignos.GetAll();
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Dignosis dropdown", MDignosMasterList.Select(x => new { x.DescriptionName, x.VisitId,x.DescriptionType }));
+        }
+
+        //List API
+        [HttpGet]
+        [Route("get-Examination")]
+        //[Permission(PageCode = "DepartmentMaster", Permission = PagePermission.View)]
+        public async Task<ApiResponse> GetExaminationDropdown()
+        {
+            var MExaminationMasterList = await _Examination.GetAll();
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Examination dropdown", MExaminationMasterList.Select(x => new { x.ExaminationId, x.ExaminationDescr }));
+        }
+        //List API
+        [HttpGet]
+        [Route("get-ChiefComplaint")]
+        //[Permission(PageCode = "DepartmentMaster", Permission = PagePermission.View)]
+        public async Task<ApiResponse> GetChiefComplaintDropdown()
+        {
+            var MChiefComplaintMasterList = await _MComplaintMaster.GetAll();
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "ChiefComplaint dropdown", MChiefComplaintMasterList.Select(x => new { x.ComplaintId, x.ComplaintDescr }));
+        }
     }
 }
 
