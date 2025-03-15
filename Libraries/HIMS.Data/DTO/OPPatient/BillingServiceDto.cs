5b08fd34d8942bc0238438ed5951aa3f3b190edb
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-03-15 14:08:33 +0530

    Service Master : GetServiceListwithGroupWise

--- Libraries/HIMS.Data/DTO/OPPatient/BillingServiceDto.cs
+++ Libraries/HIMS.Data/DTO/OPPatient/BillingServiceDto.cs
@@ -13,16 +13,26 @@ namespace HIMS.Data.DTO.OPPatient
         public string? GroupName { get; set; }
         public string? ServiceShortDesc { get; set; }
         public string? ServiceName { get; set; }
-        public double? Price { get; set; }
+        public decimal? Price { get; set; }
         public bool? IsEditable { get; set; }
         public bool? CreditedtoDoctor { get; set; }
-        public long? IsPathology { get; set; }
-        public long? IsRadiology { get; set; }
-        public bool? IsActive { get; set; }
+        public bool? IsPathology { get; set; }
+        public bool? IsRadiology { get; set; }
+        //public int? IsActive { get; set; }
         public long? PrintOrder { get; set; }
         public long? TariffId { get; set; }
         public string? TariffName { get; set; }
         public bool? IsEmergency { get; set; }
         public decimal? EmgAmt { get; set; }
    }
+
+    public class BillingServiceListDto
+    {
+        public long ServiceId { get; set; }
+        public string? ServiceName { get; set; }
+        public decimal? Price { get; set; }
+        public long? IsPathology { get; set; }
+        public long? IsRadiology { get; set; }
+        public long? TariffId { get; set; }
+    }
 }
