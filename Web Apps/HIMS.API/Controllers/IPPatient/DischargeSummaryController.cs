98d42ded388f27f40dd071ebb761834c3aa3edb8
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-03-22 00:16:14 +0530

    DischargeAPI Insert,Update/ AdmissionRegAPI INSERT

--- Web Apps/HIMS.API/Controllers/IPPatient/DischargeSummaryController.cs
+++ Web Apps/HIMS.API/Controllers/IPPatient/DischargeSummaryController.cs
@@ -177,7 +177,7 @@ namespace HIMS.API.Controllers.IPPatient
             }
             else
                 return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
-            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Discharge added successfully."+ObjAdmission.AdmissionId);
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Discharge added successfully."/*+ObjAdmission.AdmissionId*/);
         }
 
         [HttpPut("DischargeUpdate")]
