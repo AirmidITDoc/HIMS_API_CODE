ea5fca8c0c8c9e24dadaba9f3e42a285cfdb8ad9
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-03-22 17:00:13 +0530

    AdmissionServiceFile Admission Update resolve

--- Web Apps/HIMS.API/Controllers/IPPatient/AdmissionController.cs
+++ Web Apps/HIMS.API/Controllers/IPPatient/AdmissionController.cs
@@ -66,7 +66,7 @@ namespace HIMS.API.Controllers.IPPatient
 
 
         [HttpPost("AdmissionRegInsertSP")]
-        //[Permission(PageCode = "Admission", Permission = PagePermission.Add)]
+        [Permission(PageCode = "Admission", Permission = PagePermission.Add)]
         public async Task<ApiResponse> Insert(NewAdmission obj)
         {
 
