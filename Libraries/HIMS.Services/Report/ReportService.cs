25d8926b5a070c41bc865ef2affa59fba99f1728
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-04-12 10:48:18 +0530

    Reports

--- Libraries/HIMS.Services/Report/ReportService.cs
+++ Libraries/HIMS.Services/Report/ReportService.cs
@@ -1024,7 +1024,18 @@ namespace HIMS.Services.Report
                         tuple = _pdfUtility.GeneratePdfFromHtml(html, model.StorageBaseUrl, "IPDDischargewithmarkstatus", "IPDDischargewithmarkstatus", Orientation.Landscape);
                         break;
                     }
-                #endregion
+                //#region :: DischargSummary ::
+                //case "DischargSummary":
+                //    {
+                //        model.RepoertName = "Discharge Summary Receipt ";
+                //        string[] headerList = { "Sr.No", "UHID", "PatientName" };
+                //        string[] colList = { "RegNo", "PatientName"};
+                //        string htmlFilePath = Path.Combine(_hostingEnvironment.WebRootPath, "PdfTemplates", "DischargeSummary.html");
+                //        string htmlHeaderFilePath = Path.Combine(_hostingEnvironment.WebRootPath, "PdfTemplates", "NewHeader.html");
+                //        var html = GetHTMLView("m_rptDischargeSummaryPrint_New", model, htmlFilePath, htmlHeaderFilePath, colList, headerList);
+                //        tuple = _pdfUtility.GeneratePdfFromHtml(html, model.StorageBaseUrl, "DischargSummary", "DischargSummary", Orientation.Landscape);
+                //        break;
+                //    }
 
 
 
