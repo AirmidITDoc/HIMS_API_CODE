40d4ac735bf4dd984b24b086676a7b13bb78df00
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-04-14 09:41:12 +0530

    Controller Moved

--- Web Apps/HIMS.API/Controllers/Masters/Billing/ClassMasterController.cs
+++ Web Apps/HIMS.API/Controllers/Masters/Billing/ClassMasterController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using HIMS.Api.Models.Common;
 using HIMS.API.Models.Masters;
 
-namespace HIMS.API.Controllers.Masters.Personal_Information
+namespace HIMS.API.Controllers.Masters.Billing
 {
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
