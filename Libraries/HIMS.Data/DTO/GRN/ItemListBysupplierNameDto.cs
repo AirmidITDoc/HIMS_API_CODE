333b8cc863e336a00c46e06bf4d45a60072beec7
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-04-22 12:51:23 +0530

    ItemListBySupplierName

--- Libraries/HIMS.Data/DTO/GRN/ItemListBysupplierNameDto.cs
+++ Libraries/HIMS.Data/DTO/GRN/ItemListBysupplierNameDto.cs
@@ -9,34 +9,32 @@ namespace HIMS.Data.DTO.GRN
     public class ItemListBysupplierNameDto
     {
         public long GRNID { get; set; }
-        public String? GrnNumber { get; set; }
-        public String GRNDate { get; set; }
-        public String? GRNTime { get; set; }
+        public string? GrnNumber { get; set; }
+        public string GRNDate { get; set; }
+        public string? GRNTime { get; set; }
         public long? GRNDetID { get; set; }
         public long? ItemId { get; set; }
-        public String? ItemName { get; set; }
-        public String? BatchNo { get; set; }
-        public String? BatchExpDate { get; set; }
-        //public long? UOMId { get; set; }
-        //public double? MRP { get; set; }
-        //public double? Rate { get; set; }
-        //public double TotalAmount { get; set; }
-        //public long? ConversionFactor { get; set; }
-        //public double VatPer { get; set; }
-        //public double? VatAmount { get; set; }
-        //public double? DiscPercentage { get; set; }
-        //public double? DiscAmount { get; set; }
-        //public double? OtherTax { get; set; }
-        //public double? LandedRate { get; set; }
-        //public double? NetAmount { get; set; }
-        //public double? GrossAmount { get; set; }
-        //public double? BalanceQty { get; set; }
-        //public double? TotalQty { get; set; }
-
-        //public bool? IsBatchRequired { get; set; }
-        //public double? StoreId { get; set; }
-        //public double? StkID { get; set; }
-        //public double? ReturnQty { get; set; }
+        public string? ItemName { get; set; }
+        public string? BatchNo { get; set; }
+        public string? BatchExpDate { get; set; }
+        public long? UOMId { get; set; }
+        public decimal? Mrp { get; set; }
+        public decimal? Rate { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public long? ConversionFactor { get; set; }
+        public double? VatPer { get; set; }
+        public decimal? VatAmount { get; set; }
+        public double? DiscPercentage { get; set; }
+        public double? OtherTax { get; set; }
+        public decimal? LandedRate { get; set; }
+        public decimal? NetAmount { get; set; }
+        public decimal? GrossAmount { get; set; }
+        public double? BalanceQty { get; set; }
+        public float? TotalQty { get; set; }
+        public bool? IsBatchRequired { get; set; }
+        public long? StoreId { get; set; }
+        public long? StkId { get; set; }
+        public long ReturnQty { get; set; }
 
 
     }
