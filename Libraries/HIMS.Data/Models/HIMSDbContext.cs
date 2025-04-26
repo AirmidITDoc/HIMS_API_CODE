fbe65562a8565bf8a2875b62e57886808ee43b6d
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-04-26 16:55:42 +0530

    PathUnitMaster/DBContext

--- Libraries/HIMS.Data/Models/HIMSDbContext.cs
+++ Libraries/HIMS.Data/Models/HIMSDbContext.cs
@@ -515,7 +515,7 @@ namespace HIMS.Data.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=192.168.2.200;Initial Catalog=SSWeb_AIRMID_API;Persist Security Info=True;User ID=DEV001;Password=DEV001;MultipleActiveResultSets=True;Max Pool Size=5000;");
+                optionsBuilder.UseSqlServer("Data Source=192.168.2.200;Initial Catalog=SSWEB_AIRMID_API;Persist Security Info=True;User ID=DEV001;Password=DEV001;MultipleActiveResultSets=True;Max Pool Size=5000;");
             }
         }
 
@@ -7319,6 +7319,8 @@ namespace HIMS.Data.Models
                 entity.HasKey(e => e.PackageId);
 
                 entity.ToTable("M_PackageDetails");
+
+                entity.Property(e => e.PackageId).ValueGeneratedNever();
 
                 entity.Property(e => e.Price)
                     .HasColumnType("money")
