39bad7bb01c88682cfd20df48faaea0c52482d0f
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2025-05-20 19:22:36 +0530

    SalesSaveWithCredit,SalesSaveWithPayment Done

--- Web Apps/HIMS.API/Controllers/Pharmacy/SalesController.cs
+++ Web Apps/HIMS.API/Controllers/Pharmacy/SalesController.cs
@@ -2,6 +2,7 @@
 using HIMS.Api.Controllers;
 using HIMS.Api.Models.Common;
 using HIMS.API.Extensions;
+using HIMS.API.Models.Inventory;
 using HIMS.API.Models.Masters;
 using HIMS.API.Models.Pharmacy;
 using HIMS.Core;
@@ -53,7 +54,7 @@ namespace HIMS.API.Controllers.Pharmacy
             return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Sales added successfully.");
         }
 
-      
+
         [HttpPost("SalesSummaryList")]
         [Permission(PageCode = "Sales", Permission = PagePermission.View)]
         public async Task<IActionResult> SalesSummaryList(GridRequestModel objGrid)
@@ -88,6 +89,59 @@ namespace HIMS.API.Controllers.Pharmacy
             return Ok(SalesDetailsList.ToGridResponse(objGrid, "Sales Bill List"));
         }
 
-     
+
+        // done by Ashu Date : 20-May-2025
+
+        [HttpPost("SalesSaveWithPayment")]
+        [Permission(PageCode = "Sales", Permission = PagePermission.Add)]
+
+        public async Task<ApiResponse> InsertSP(SaleReqModel obj)
+        {
+            TSalesHeader model = obj.Sales.MapTo<TSalesHeader>();
+            List<TCurrentStock> CurrentStock = obj.TCurrentStock.MapTo<List<TCurrentStock>>();
+            Payment modelPayment = obj.Payment.MapTo<Payment>();
+            TIpPrescription modelPrescription = obj.Prescription.MapTo<TIpPrescription>();
+            TSalesDraftHeader modelDraftHeader = obj.SalesDraft.MapTo<TSalesDraftHeader>();
+
+
+            if (obj.Sales.SalesId == 0)
+            {
+                model.Date = Convert.ToDateTime(obj.Sales.Date);
+                model.Time = Convert.ToDateTime(obj.Sales.Time);
+                model.AddedBy = CurrentUserId;
+                await _ISalesService.InsertAsyncSP(model, CurrentStock, modelPayment, modelPrescription, modelDraftHeader, CurrentUserId, CurrentUserName);
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "SalesSaveWithPayment added successfully.");
+        }
+
+
+
+        // done by Ashu Date : 20-May-2025
+
+        [HttpPost("SalesSaveWithCredit")]
+        [Permission(PageCode = "Sales", Permission = PagePermission.Add)]
+        public async Task<ApiResponse> InsertSPC(SaleReqModel obj)
+        {
+            TSalesHeader model = obj.Sales.MapTo<TSalesHeader>();
+            List<TCurrentStock> CurrentStock = obj.TCurrentStock.MapTo<List<TCurrentStock>>();
+            //   Payment modelPayment = obj.Payment.MapTo<Payment>();
+            TIpPrescription modelPrescription = obj.Prescription.MapTo<TIpPrescription>();
+            TSalesDraftHeader modelDraftHeader = obj.SalesDraft.MapTo<TSalesDraftHeader>();
+
+            if (obj.Sales.SalesId == 0)
+            {
+                model.Date = Convert.ToDateTime(obj.Sales.Date);
+                model.Time = Convert.ToDateTime(obj.Sales.Time);
+                model.AddedBy = CurrentUserId;
+                await _ISalesService.InsertAsyncSPC(model, CurrentStock, modelPrescription, modelDraftHeader, CurrentUserId, CurrentUserName);
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "SalesSaveWithCredit added successfully.");
+        }
+
     }
+
 }
