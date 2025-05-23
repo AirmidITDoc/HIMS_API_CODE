6bb283ebe6718dceb61ea067f76b3ff2d41aa323
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2025-05-23 15:30:18 +0530

    NursingConsentController

---
+++ Libraries/HIMS.Services/Nursing/INursingConsentService.cs
@@ -0,0 +1,22 @@
+﻿using HIMS.Core.Domain.Grid;
+using HIMS.Data.DTO.IPPatient;
+using HIMS.Data.DTO.Nursing;
+using HIMS.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HIMS.Services.Nursing
+{
+    public partial interface INursingConsentService
+    {
+    //    Task InsertAsync(TConsentInformation ObjTConsentInformation, int UserId, string Username);
+        Task InsertAsync(TConsentInformation ObjTConsentInformation, int UserId, string Username);
+
+        Task UpdateAsync(TConsentInformation ObjTConsentInformation, int UserId, string Username);
+
+
+
+    }
+}
