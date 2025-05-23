9ba70c81c42cbc6010261ebeefebe64a9525583b
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-05-23 14:24:19 +0530

    TConsentInformation/ Ashutosh

---
+++ Libraries/HIMS.Data/Models/TConsentInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HIMS.Data.Models
+{
+    public partial class TConsentInformation
+    {
+        public long ConsentId { get; set; }
+        public DateTime? ConsentDate { get; set; }
+        public DateTime? ConsentTime { get; set; }
+        public long? Opipid { get; set; }
+        public long? Opiptype { get; set; }
+        public long? ConsentDeptId { get; set; }
+        public long? ConsentTempId { get; set; }
+        public string? ConsentName { get; set; }
+        public string? ConsentText { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? CreatedDatetime { get; set; }
+        public int? ModifiedBy { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+    }
+}
