8dbd59648875bdf1cc3201cc993450db676fdc8e
Vimal Vataliya <elaunch.vimal@gmail.com>
2025-06-10 21:39:16 +0530

    set search based on exact data come first

--- Libraries/HIMS.Services/OPPatient/RegistrationService.cs
+++ Libraries/HIMS.Services/OPPatient/RegistrationService.cs
@@ -30,7 +30,7 @@ namespace HIMS.Services.OPPatient
         public virtual async Task<List<RegistrationAutoCompleteDto>> SearchRegistration(string str)
         {
 
-            var qry = this._context.Registrations
+            return await this._context.Registrations
                 .Where(x =>
                     (x.FirstName + " " + x.LastName).ToLower().StartsWith(str) || // Optional: if you want full name search
                     x.FirstName.ToLower().StartsWith(str) ||                     // Match first name starting with str
@@ -50,14 +50,9 @@ namespace HIMS.Services.OPPatient
                     AgeMonth = x.AgeMonth,
                     AgeDay = x.AgeDay
 
-                });
-            if (str.ToInt() > 0)
-                qry = qry.OrderBy(x => x.RegNo).ThenBy(x => x.MobileNo);
-            else
-                qry = qry.OrderBy(x => x.FirstName);
-
-            return await qry.ToListAsync();
-
+                })
+               .OrderByDescending(x => x.RegNo == str ? 3 : x.MobileNo == str ? 2 : (x.FirstName + " " + x.LastName) == str ? 1 : 0)
+               .ThenBy(x => x.FirstName).ToListAsync();
 
         }
 
