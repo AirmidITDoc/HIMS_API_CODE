dd97139821ec9ee7c6efb10f02f26e0c08869ac6
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-07-15 12:34:24 +0530

    VisitDetailsService

--- Libraries/HIMS.Services/OutPatient/VisitDetailsService.cs
+++ Libraries/HIMS.Services/OutPatient/VisitDetailsService.cs
@@ -105,7 +105,7 @@ namespace HIMS.Services.OutPatient
         {
             // OLD CODE With SP
             DatabaseHelper odal = new();
-            string[] rEntity = { "RegNo", "UpdatedBy", "RegPrefix", "AnnualIncome", "IsIndientOrWeaker", "RationCardNo", "IsMember" };
+            string[] rEntity = { "RegNo", "RegPrefix", "AnnualIncome", "IsIndientOrWeaker", "RationCardNo", "IsMember", "UpdatedBy", "CreatedBy", "CreatedDate", "ModifiedBy", "ModifiedDate" };
             var entity = objRegistration.ToDictionary();
             foreach (var rProperty in rEntity)
             {
--- Web Apps/HIMS.API/Models/OutPatient/AppReistrationModel.cs
+++ Web Apps/HIMS.API/Models/OutPatient/AppReistrationModel.cs
@@ -4,7 +4,6 @@ namespace HIMS.API.Models.OutPatient
 {
     public class AppReistrationModel
     {
-        public long RegId { get; set; }
         public DateTime? RegDate { get; set; }
         public string? RegTime { get; set; }
         public long? PrefixId { get; set; }
@@ -14,9 +13,9 @@ namespace HIMS.API.Models.OutPatient
         public string? Address { get; set; }
         public string? City { get; set; }
         public string? PinNo { get; set; }
-        public string? DateofBirth { get; set; }
+        public DateTime? DateofBirth { get; set; }
         public string? Age { get; set; }
-        public long? GenderID { get; set; }
+        public long? GenderId { get; set; }
         public string? PhoneNo { get; set; }
         public string? MobileNo { get; set; }
         public long? AddedBy { get; set; }
@@ -34,6 +33,24 @@ namespace HIMS.API.Models.OutPatient
         public string? AadharCardNo { get; set; }
         public string? PanCardNo { get; set; }
         public string? Photo { get; set; }
+        public string? EmgContactPersonName { get; set; }
+        public long? EmgRelationshipId { get; set; }
+        public string? EmgMobileNo { get; set; }
+        public string? EmgLandlineNo { get; set; }
+        public string? EngAddress { get; set; }
+        public string? EmgAadharCardNo { get; set; }
+        public string? EmgDrivingLicenceNo { get; set; }
+        public string? MedTourismPassportNo { get; set; }
+        public DateTime? MedTourismVisaIssueDate { get; set; }
+        public DateTime? MedTourismVisaValidityDate { get; set; }
+        public string? MedTourismNationalityId { get; set; }
+        public long? MedTourismCitizenship { get; set; }
+        public string? MedTourismPortOfEntry { get; set; }
+        public DateTime? MedTourismDateOfEntry { get; set; }
+        public string? MedTourismResidentialAddress { get; set; }
+        public string? MedTourismOfficeWorkAddress { get; set; }
+        public long RegId { get; set; }
+
 
     }
     public class AppReistrationModelValidator : AbstractValidator<AppReistrationModel>
@@ -43,7 +60,6 @@ namespace HIMS.API.Models.OutPatient
             RuleFor(x => x.PrefixId).NotNull().NotEmpty().WithMessage("Prefix is required");
             RuleFor(x => x.FirstName).NotNull().NotEmpty().WithMessage("FirstName is required");
             RuleFor(x => x.LastName).NotNull().NotEmpty().WithMessage("LastName is required");
-            RuleFor(x => x.GenderID).NotNull().NotEmpty().WithMessage("Gender is required");
             RuleFor(x => x.CityId).NotNull().NotEmpty().WithMessage("City is required");
             RuleFor(x => x.StateId).NotNull().NotEmpty().WithMessage("State is required");
             RuleFor(x => x.CountryId).NotNull().NotEmpty().WithMessage("Country is required");
