7ef1d73359140b93aa40626fcdd5c1afe83c6031
Vimal Vataliya <elaunch.vimal@gmail.com>
2025-07-17 21:57:23 +0530

    Add version in api

--- Web Apps/HIMS.API/Controllers/Common/FilesController.cs
+++ Web Apps/HIMS.API/Controllers/Common/FilesController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.StaticFiles;
 
 namespace HIMS.API.Controllers.Common
 {
-    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class FilesController : BaseController
     {
