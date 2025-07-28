d72140c47a289bd0cf12bf0f7173ce38a3a249d8
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2025-07-28 15:12:06 +0530

    LoginUnitUserWiseListDto

---
+++ Libraries/HIMS.Services/Inventory/LoginUnitUserWiseListDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HIMS.Services.Inventory
+{
+    public class LoginUnitUserWiseListDto
+    {
+        public long? LoginId { get; set; }
+        public long? UnitId { get; set; }
+        public string? HospitalName { get; set; }
+
+    }
+}
