d5f3d734781ed2de82700cd8ebb972b825f8354c
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-08-21 20:28:50 +0530

    Menu Orders

--- Libraries/HIMS.Services/Users/MenuService.cs
+++ Libraries/HIMS.Services/Users/MenuService.cs
@@ -23,7 +23,7 @@ namespace HIMS.Services.Users
             DatabaseHelper sql=new();
             SqlParameter[] para = new SqlParameter[1];
             para[0] = new SqlParameter("@RoleId", RoleId);
-            List<MenuMasterDTO> lstMenu = sql.FetchListByQuery<MenuMasterDTO>("SELECT M.Id,ISNULL(M.UpId,0) UpId,M.LinkName,M.Icon,M.LinkAction,M.SortOrder,M.IsActive,M.IsDisplay,M.PermissionCode,M.TableNames,P.IsView,P.IsAdd,P.IsEdit,P.IsDelete FROM MenuMaster M LEFT JOIN PermissionMaster P ON M.Id=P.MenuId AND P.RoleId=@RoleId\r\nWHERE IsActive=1 AND IsDisplay=1", para);
+            List<MenuMasterDTO> lstMenu = sql.FetchListByQuery<MenuMasterDTO>("SELECT M.Id,ISNULL(M.UpId,0) UpId,M.LinkName,M.Icon,M.LinkAction,M.SortOrder,M.IsActive,M.IsDisplay,M.PermissionCode,M.TableNames,P.IsView,P.IsAdd,P.IsEdit,P.IsDelete FROM MenuMaster M LEFT JOIN PermissionMaster P ON M.Id=P.MenuId AND P.RoleId=@RoleId\r\nWHERE IsActive=1 AND IsDisplay=1 Order by UpId,SortOrder ", para);
             return PrepareMenu(lstMenu, isActiveMenuOnly);
         }
         public static List<MenuModel> PrepareMenu(List<MenuMasterDTO> lstMenu, bool isActiveMenuOnly)
