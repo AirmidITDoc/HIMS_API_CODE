d32c14ee4b2a912a378483471141b3b72e40bb14
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-08-28 13:01:33 +0530

    MReportConfigList

--- Libraries/HIMS.Data/DTO/Administration/MReportConfigListDto.cs
+++ Libraries/HIMS.Data/DTO/Administration/MReportConfigListDto.cs
@@ -32,5 +32,8 @@ namespace HIMS.Data.DTO.Administration
         public string ReportFilter { get; set; }
         public bool IsActive { get; set; }
         public long? MenuId { get; set; }
+        public string ReportSummary { get; set; }
+
+        
     }
 }
