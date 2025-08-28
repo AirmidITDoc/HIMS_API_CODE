7a0c493bc308eb66ed82fa3a77d41bbf0697fcec
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-08-28 10:36:08 +0530

    RequestForIPListDto

--- Libraries/HIMS.Data/DTO/IPPatient/RequestForIPListDto.cs
+++ Libraries/HIMS.Data/DTO/IPPatient/RequestForIPListDto.cs
@@ -9,7 +9,7 @@ namespace HIMS.Data.DTO.IPPatient
     public  class RequestForIPListDto
     {
         public string PatientName {  get; set; }
-        public string AgeYear { get; set; }
+        public string AgeYear { get; set; } 
         public string GenderName { get; set; }
         public string VisitDate { get; set; }
         public string VisitTime { get; set; }
