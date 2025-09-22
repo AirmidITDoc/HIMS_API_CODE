c075cb750711d28a73725669afb8a3739d27c7ed
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-09-22 14:46:31 +0530

    GRN  Return update model chnages done

--- Web Apps/HIMS.API/Models/Pharmacy/GRNUpdateModel.cs
+++ Web Apps/HIMS.API/Models/Pharmacy/GRNUpdateModel.cs
@@ -26,10 +26,8 @@ namespace HIMS.API.Models.Pharmacy
         public string? GrnType { get; set; }
         public bool IsGrnTypeFlag { get; set; }
         public long GrnreturnId { get; set; }
+        public long UnitId { get; set; }
         //public List<GRNReturnDetailsModel> TGrnreturnDetails { get; set; }
-
-
-
 
     }
     public class GrnreturnUpdate
@@ -47,12 +45,8 @@ namespace HIMS.API.Models.Pharmacy
         public decimal? LandedRate { get; set; }
         public decimal? Mrp { get; set; }
         public decimal? UnitPurchaseRate { get; set; }
-        public float? VatPercentage { get; set; }
-        public decimal? VatAmount { get; set; }
-        public decimal? TaxAmount { get; set; }
-        public decimal? OtherTaxAmount { get; set; }
-        public float? OctroiPer { get; set; }
-        public decimal? OctroiAmt { get; set; }
+        public float? GSTPercentage { get; set; }
+        public decimal? GSTAmount { get; set; }
         public decimal? LandedTotalAmount { get; set; }
         public decimal? MrptotalAmount { get; set; }
         public decimal? PurchaseTotalAmount { get; set; }
@@ -62,6 +56,11 @@ namespace HIMS.API.Models.Pharmacy
         public float? Cf { get; set; }
         public float? TotalQty { get; set; }
         public long? Grnid { get; set; }
+        public float? Cgstper { get; set; }
+        public float? Sgstper { get; set; }
+        public float? Igstper { get; set; }
+        public float? DiscPercentage { get; set; }
+        public decimal? DiscAmount { get; set; }
     }
     public class CurStockModels
     {
