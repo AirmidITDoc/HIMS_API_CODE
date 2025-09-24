cdb3804237d5f49f6e5d86450f5437905f79c1e0
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-09-24 17:33:14 +0530

    MExpensesHeadMaster save,update,cancel,list

---
+++ Web Apps/HIMS.API/Controllers/Administration/ExpensesHeadMasterController.cs
@@ -0,0 +1,101 @@
+﻿using Asp.Versioning;
+using HIMS.Api.Controllers;
+using HIMS.Data.Models;
+using HIMS.Data;
+using Microsoft.AspNetCore.Mvc;
+using HIMS.API.Extensions;
+using HIMS.Core.Domain.Grid;
+using HIMS.Core;
+using HIMS.API.Models.Administration;
+using HIMS.Api.Models.Common;
+using HIMS.API.Models.Masters;
+
+namespace HIMS.API.Controllers.Administration
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class ExpensesHeadMasterController : BaseController
+    {
+        private readonly IGenericService<MExpensesHeadMaster> _repository;
+        public ExpensesHeadMasterController(IGenericService<MExpensesHeadMaster> repository)
+        {
+            _repository = repository;
+        }
+        //List API
+        [HttpPost]
+        [Route("[action]")]
+        //[Permission(PageCode = "PatientType", Permission = PagePermission.View)]
+        public async Task<IActionResult> List(GridRequestModel objGrid)
+        {
+            IPagedList<MExpensesHeadMaster> PatientTypeList = await _repository.GetAllPagedAsync(objGrid);
+            return Ok(PatientTypeList.ToGridResponse(objGrid, "PatientType List"));
+        }
+        //List API Get By Id
+        [HttpGet("{id?}")]
+        //[Permission(PageCode = "PatientType", Permission = PagePermission.View)]
+        public async Task<ApiResponse> Get(int id)
+        {
+            if (id == 0)
+            {
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status400BadRequest, "No data found.");
+            }
+            var data = await _repository.GetById(x => x.ExpHedId == id);
+            return data.ToSingleResponse<MExpensesHeadMaster, ExpensesHeadModel>("PatientType");
+        }
+        //Add API
+        [HttpPost]
+        //[Permission(PageCode = "PatientType", Permission = PagePermission.Add)]
+        public async Task<ApiResponse> Post(ExpensesHeadModel obj)
+        {
+            MExpensesHeadMaster model = obj.MapTo<MExpensesHeadMaster>();
+            model.IsActive = true;
+            if (obj.ExpHedId == 0)
+            {
+                model.CreatedBy = CurrentUserId;
+                model.AddedBy = CurrentUserId;
+                model.CreatedDate = DateTime.Now;
+                await _repository.Add(model, CurrentUserId, CurrentUserName);
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Record  added successfully.");
+        }
+        //Edit API
+        [HttpPut("{id:int}")]
+        //[Permission(PageCode = "PatientType", Permission = PagePermission.Edit)]
+        public async Task<ApiResponse> Edit(ExpensesHeadModel obj)
+        {
+            MExpensesHeadMaster model = obj.MapTo<MExpensesHeadMaster>();
+            model.IsActive = true;
+            if (obj.ExpHedId == 0)
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            else
+            {
+                model.ModifiedBy = CurrentUserId;
+                model.UpdatedBy = CurrentUserId;
+                model.ModifiedDate = DateTime.Now;
+                await _repository.Update(model, CurrentUserId, CurrentUserName, new string[2] { "CreatedBy", "CreatedDate" });
+            }
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Record  updated successfully.");
+        }
+        //Delete API
+        [HttpDelete]
+        //[Permission(PageCode = "PatientType", Permission = PagePermission.Delete)]
+        public async Task<ApiResponse> Delete(int Id)
+        {
+            MExpensesHeadMaster model = await _repository.GetById(x => x.ExpHedId == Id);
+            if ((model?.ExpHedId ?? 0) > 0)
+            {
+                model.IsActive = false;
+                model.ModifiedBy = CurrentUserId;
+                model.ModifiedDate = DateTime.Now;
+                await _repository.SoftDelete(model, CurrentUserId, CurrentUserName);
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Record  deleted successfully.");
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+        }
+
+    }
+}
---
+++ Web Apps/HIMS.API/Models/Administration/ExpensesHeadModel.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using HIMS.API.Models.Masters;
+
+namespace HIMS.API.Models.Administration
+{
+    public class ExpensesHeadModel
+    {
+        public long ExpHedId { get; set; }
+        public string? HeadName { get; set; }
+       
+    }
+    public class PatientTypeModelValidator : AbstractValidator<PatientTypeModel>
+    {
+        public PatientTypeModelValidator()
+        {
+            //RuleFor(x => x.HeadName).NotNull().NotEmpty().WithMessage("HeadName  is required");
+        }
+    }
+}
