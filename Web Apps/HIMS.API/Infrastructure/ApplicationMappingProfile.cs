22529f62e28f69b0330eb52e74103fad608a7e53
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-10-06 18:18:44 +0530

    Application mapping

--- Web Apps/HIMS.API/Infrastructure/ApplicationMappingProfile.cs
+++ Web Apps/HIMS.API/Infrastructure/ApplicationMappingProfile.cs
@@ -211,7 +211,6 @@ namespace HIMS.API.Infrastructure
             CreateMap<TCurrentStock, materialAcceptStockUpdate>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<TCurrentStock, CurrentStockUpdateModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<TMaterialConsumptionHeader, MaterialConsumptionHeader>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
-            CreateMap<RoomMaster, WardMasterModels>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
 
 
 
