e1007fa6c864dc3a70a52376e3ab4af79713000b
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-10-06 13:27:53 +0530

    ReportTemplateListDto

--- Libraries/HIMS.Data/DTO/Administration/ReportTemplateListDto.cs
+++ Libraries/HIMS.Data/DTO/Administration/ReportTemplateListDto.cs
@@ -11,5 +11,14 @@ namespace HIMS.Data.DTO.Administration
         public long TemplateId { get; set; }
         public string? TemplateName { get; set; }
         public string? TemplateDescription { get; set; }
+        public bool? IsActive { get; set; }
+        public long? DepartmentId { get; set; }
+        public string? CategoryName { get; set; }
+        public string? TemplateHeader { get; set; }
+        public string? TemplateFooter { get; set; }
+        public bool? IsTemplateWithHeader { get; set; }
+        public bool? IsTemplateHeaderWithImage { get; set; }
+        public bool? IsTemplateWithFooter { get; set; }
+        public bool? IsTemplateFooterWithImage { get; set; }
     }
 }
