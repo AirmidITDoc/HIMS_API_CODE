385df6feffdaf200ec26d249609c08fdc39e7fad
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2025-10-30 20:53:12 +0530

    PatientWiseAdvanceList

--- Libraries/HIMS.Data/DTO/IPPatient/PatientWiseAdvanceListDto.cs
+++ Libraries/HIMS.Data/DTO/IPPatient/PatientWiseAdvanceListDto.cs
@@ -3,7 +3,7 @@
     public class PatientWiseAdvanceListDto
     {
         public long AdvanceDetailID { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public long AdvanceId { get; set; }
         public string AdvanceNo { get; set; }
         public decimal AdvanceAmount { get; set; }
@@ -12,14 +12,16 @@
         public decimal RefundAmount { get; set; }
         public long OPD_IPD_Id { get; set; }
         public string UserName { get; set; }
-        public DateTime PaymentDate { get; set; }
+        public string PaymentDate { get; set; }
         public decimal CashPayAmount { get; set; }
         public decimal ChequePayAmount { get; set; }
         public decimal CardPayAmount { get; set; }
-        public decimal NEFTPayAmount { get; set; }
-        public decimal PayTMAmount { get; set; }
+        public decimal OnlineAmount { get; set; }
         public long TransactionType { get; set; }
         public string Reason { get; set; }
+        public string Time { get; set; }
+        public string PaymentTime { get; set; }
+
 
 
     }
