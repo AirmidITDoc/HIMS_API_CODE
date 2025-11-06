cb8a3212f3a99835b77f8f9e6021d6842e3a4719
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2025-11-06 16:53:01 +0530

    Labreglist,billdetail,Itemgeneric

---
+++ Libraries/HIMS.Data/DTO/Pathology/LabregBilldetailListDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.Pathology
+{
+    public class LabregBilldetailListDto
+    {
+        public string? BillNo { get; set; }
+        public DateTime BillTime { get; set; }
+        public long ChargesId { get; set; }
+        public string ServiceName { get; set; }
+
+
+        public DateTime ChargesTime { get; set; }
+        public double Price { get; set; }
+        public long? IsPathology { get; set; }
+        public long? IsRadiology { get; set; }
+        public string? DoctorName { get; set; }
+
+        //public decimal? CardPay { get; set; }
+        //public decimal? OnlinePay { get; set; }
+    }
+}
--- Web Apps/HIMS.API/Controllers/Masters/Personal Information/GenericMasterController.cs
+++ Web Apps/HIMS.API/Controllers/Masters/Personal Information/GenericMasterController.cs
@@ -16,8 +16,8 @@ namespace HIMS.API.Controllers.Masters.Personal_Information
     [ApiVersion("1")]
     public class GenericMasterController : BaseController
     {
-        private readonly IGenericService<MGenericMaster> _repository;
-        public GenericMasterController(IGenericService<MGenericMaster> repository)
+        private readonly IGenericService<MItemGenericNameMaster> _repository;
+        public GenericMasterController(IGenericService<MItemGenericNameMaster> repository)
         {
             _repository = repository;
         }
@@ -25,32 +25,32 @@ namespace HIMS.API.Controllers.Masters.Personal_Information
         //List API
         [HttpPost]
         [Route("[action]")]
-        [Permission(PageCode = "GenericMaster", Permission = PagePermission.View)]
+        //[Permission(PageCode = "GenericMaster", Permission = PagePermission.View)]
         public async Task<IActionResult> List(GridRequestModel objGrid)
         {
-            IPagedList<MGenericMaster> MGenericMasterList = await _repository.GetAllPagedAsync(objGrid);
+            IPagedList<MItemGenericNameMaster> MGenericMasterList = await _repository.GetAllPagedAsync(objGrid);
             return Ok(MGenericMasterList.ToGridResponse(objGrid, "Generic List"));
         }
         //List API Get By Id
         [HttpGet("{id?}")]
-        [Permission(PageCode = "GenericMaster", Permission = PagePermission.View)]
+        //[Permission(PageCode = "GenericMaster", Permission = PagePermission.View)]
         public async Task<ApiResponse> Get(int id)
         {
             if (id == 0)
             {
                 return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status400BadRequest, "No data found.");
             }
-            var data = await _repository.GetById(x => x.GenericId == id);
-            return data.ToSingleResponse<MGenericMaster, GenericMasterModel>("Generic");
+            var data = await _repository.GetById(x => x.ItemGenericNameId == id);
+            return data.ToSingleResponse<MItemGenericNameMaster, GenericMasterModel>("Generic");
         }
         //Add API
         [HttpPost]
-        [Permission(PageCode = "GenericMaster", Permission = PagePermission.Add)]
+        //[Permission(PageCode = "GenericMaster", Permission = PagePermission.Add)]
         public async Task<ApiResponse> Post(GenericMasterModel obj)
         {
-            MGenericMaster model = obj.MapTo<MGenericMaster>();
+            MItemGenericNameMaster model = obj.MapTo<MItemGenericNameMaster>();
             model.IsActive = true;
-            if (obj.GenericId == 0)
+            if (obj.ItemGenericNameId == 0)
             {
                 model.CreatedBy = CurrentUserId;
                 model.CreatedDate = DateTime.Now;
@@ -62,12 +62,12 @@ namespace HIMS.API.Controllers.Masters.Personal_Information
         }
         //Edit API
         [HttpPut("{id:int}")]
-        [Permission(PageCode = "GenericMaster", Permission = PagePermission.Edit)]
+        //[Permission(PageCode = "GenericMaster", Permission = PagePermission.Edit)]
         public async Task<ApiResponse> Edit(GenericMasterModel obj)
         {
-            MGenericMaster model = obj.MapTo<MGenericMaster>();
+            MItemGenericNameMaster model = obj.MapTo<MItemGenericNameMaster>();
             model.IsActive = true;
-            if (obj.GenericId == 0)
+            if (obj.ItemGenericNameId == 0)
                 return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
             else
             {
@@ -79,11 +79,11 @@ namespace HIMS.API.Controllers.Masters.Personal_Information
         }
         //Delete API
         [HttpDelete]
-        [Permission(PageCode = "GenericMaster", Permission = PagePermission.Delete)]
+        //[Permission(PageCode = "GenericMaster", Permission = PagePermission.Delete)]
         public async Task<ApiResponse> Delete(int Id)
         {
-            MGenericMaster model = await _repository.GetById(x => x.GenericId == Id);
-            if ((model?.GenericId ?? 0) > 0)
+            MItemGenericNameMaster model = await _repository.GetById(x => x.ItemGenericNameId == Id);
+            if ((model?.ItemGenericNameId ?? 0) > 0)
             {
                 model.IsActive = model.IsActive == true ? false : true;
                 model.ModifiedBy = CurrentUserId;
