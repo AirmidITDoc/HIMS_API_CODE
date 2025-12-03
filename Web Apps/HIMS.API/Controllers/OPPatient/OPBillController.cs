43e8df33f208bef5d3577ca6b7591195719085e6
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-12-03 13:32:42 +0530

    OPBillingInsert

--- Web Apps/HIMS.API/Controllers/OPPatient/OPBillController.cs
+++ Web Apps/HIMS.API/Controllers/OPPatient/OPBillController.cs
@@ -83,7 +83,7 @@ namespace HIMS.API.Controllers.OPPatient
         }
 
         [HttpPost("OPBillingInsert")]
-        [Permission(PageCode = "Bill", Permission = PagePermission.Add)]
+        //[Permission(PageCode = "Bill", Permission = PagePermission.Add)]
         public async Task<ApiResponse> Insert(OPBillIngModel obj)
         {
             Bill model = obj.MapTo<Bill>();
