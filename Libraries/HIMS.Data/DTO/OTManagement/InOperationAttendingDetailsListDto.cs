314c4d9e3b77ca8b954ccf50db0c790340eb26e9
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-12-05 13:10:44 +0530

    InOperationAttendingDetailsList/InOperationSurgeryDetailsList

--- Libraries/HIMS.Data/DTO/OTManagement/InOperationAttendingDetailsListDto.cs
+++ Libraries/HIMS.Data/DTO/OTManagement/InOperationAttendingDetailsListDto.cs
@@ -8,33 +8,30 @@ namespace HIMS.Data.DTO.OTManagement
 {
     public  class InOperationAttendingDetailsListDto
     {
-        public long OtinOperationAttendingDetId { get; set; }
         public long? OtinOperationId { get; set; }
         public long? DoctorTypeId { get; set; }
         public long? DoctorId { get; set; }
         public int? SeqNo { get; set; }
-        public long? Createdby { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? ModifiedDate { get; set; }
-        public int? ModifiedBy { get; set; }
+        public string? DoctorType { get; set; }
+        public string? DoctorName { get; set; }
+       
     }
     public class InOperationSurgeryDetailsDto
     {
-        public long OtinOperationSurgeryDetId { get; set; }
         public long? OtinOperationId { get; set; }
         public long? SurgeryCategoryId { get; set; }
         public long? SurgeryId { get; set; }
         public string? SurgeryPart { get; set; }
+        public string? SurgeryCategoryName { get; set; }
+        public string? SurgeryName { get; set; }
         public DateTime? SurgeryFromTime { get; set; }
         public DateTime? SurgeryEndTime { get; set; }
         public int? SurgeryDuration { get; set; }
         public string? IsPrimary { get; set; }
         public long? SurgeonId { get; set; }
         public long? AnesthetistId { get; set; }
-        public int? SeqNo { get; set; }
-        public long? Createdby { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? ModifiedDate { get; set; }
-        public int? ModifiedBy { get; set; }
+        public string? SurgeonName { get; set; }
+        public string? AnestheticsName { get; set; }
+      
     }
 }
