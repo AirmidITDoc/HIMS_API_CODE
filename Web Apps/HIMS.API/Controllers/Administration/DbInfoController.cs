12a26201c86dd7aa519e9a07185e58e7490f6c7e
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2025-12-24 15:28:16 +0530

    DB info connected

---
+++ Web Apps/HIMS.API/Controllers/Administration/DbInfoController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+using HIMS.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace HIMS.API.Controllers.Administration
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class DbInfoController : BaseController
+    {
+        private readonly IConfiguration _configuration;
+        public DbInfoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetDbInfo()
+        {
+
+            var connStr = _configuration.GetConnectionString("CONNECTION_STRING");
+            var builder = new SqlConnectionStringBuilder(connStr);
+
+            return Ok(new
+            {
+                Server = builder.DataSource,
+                Database = builder.InitialCatalog,
+                Provider = "SQL Server"
+            });
+        }
+    }
+}
