dbb938b898602fb7737d2f61fd62867dab90dfa5
uday palli <udaygopalpalli@gmail.com>
2025-12-26 15:42:08 +0530

    Lab Browse Api Created

---
+++ Libraries/HIMS.Data/DTO/Pathology/LabBrowsePaymentListDto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.Pathology
+{
+    public class LabBrowsePaymentListDto
+    {
+        public string? RegNo { get; set; }
+        public string? PatientName { get; set; }
+        public string? AgeYear { get; set; }
+        public string? MobileNo { get; set; }
+
+        public string? DoctorName { get; set; }
+        public string? RefDoctorName { get; set; }
+
+        public string? OPDNo { get; set; }
+        //public string? VisitDate { get; set; }
+
+        public string? HospitalName { get; set; }
+        public string? CompanyName { get; set; }
+        public long? CompanyId { get; set; }
+
+        public string? PBillNo { get; set; }
+        public DateTime? BillDate { get; set; }
+        public decimal? BillAmount { get; set; }
+        public string? DiscAmount { get; set; }
+        public decimal? NetAmount { get; set; }
+
+   
+        public decimal? PaidAmount { get; set; }
+        public decimal? BalanceAmt { get; set; }
+
+        public DateTime? PaymentDate { get; set; }
+        public DateTime? PaymentTime { get; set; }
+        public string? ReceiptNo { get; set; }
+
+  
+        public decimal? CashPayAmount { get; set; }
+        public decimal? ChequePayAmount { get; set; }
+        public decimal? CardPayAmount { get; set; }
+        public decimal? AdvanceUsedAmount { get; set; }
+        public decimal? NEFTPayAmount { get; set; }
+        public decimal? PayTMAmount { get; set; }
+        public decimal? OnlinePay { get; set; }
+
+        public string? PaymentId { get; set; }
+        public string? UserName { get; set; }
+    }
+}
---
+++ Libraries/HIMS.Data/DTO/Pathology/LabBrowseRefundListDto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.Pathology
+{
+    public class LabBrowseRefundListDto
+    {
+
+     
+        public long? RefundId { get; set; }
+        public DateTime? RefundDate { get; set; }
+        public string? RefundTime { get; set; }
+        public string? RefundNo { get; set; }
+
+        public string? PBillNo { get; set; }
+        //public long? OPD_IPD_Type { get; set; }
+        //public long? OPD_IPD_ID { get; set; }
+
+        public string? RegNo { get; set; }
+        public DateTime? RegDate { get; set; }
+        public string? PatientName { get; set; }
+        public string? PatientType { get; set; }
+        public string? MobileNo { get; set; }
+
+        public string? DoctorName { get; set; }
+        public string? RefDoctorName { get; set; }
+
+        public string? TariffName { get; set; }
+        public string? CompanyName { get; set; }
+        public string? HospitalName { get; set; }
+
+        public double? TotalAmt { get; set; }
+        public double? ConcessionAmt { get; set; }
+        public double? NetPayableAmt { get; set; }
+        public double? BalanceAmt { get; set; }
+        public double? RefundAmount { get; set; }
+
+
+        public long? PaymentId { get; set; }
+        public DateTime? PaymentDate { get; set; }
+        public DateTime? PaymentTime { get; set; }
+
+        public double? CashPayAmount { get; set; }
+        public double? ChequePayAmount { get; set; }
+        public double? CardPayAmount { get; set; }
+        public double? AdvanceUsedAmount { get; set; }
+
+
+        //public long? TransactionId { get; set; }
+       // public long? TransactionType { get; set; }
+       // public long? BillId { get; set; }
+
+       // public string? PaymentType { get; set; }
+
+        public string? Remark { get; set; }
+        public string? PaymentRemark { get; set; }
+        public string? AddedBy { get; set; }
+    }
+}
