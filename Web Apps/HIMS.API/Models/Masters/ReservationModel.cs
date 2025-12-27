979d196795d04bc58217ad3c7546964871703685
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2025-12-27 19:30:04 +0530

    ReservationModel

--- Web Apps/HIMS.API/Models/Masters/ReservationModel.cs
+++ Web Apps/HIMS.API/Models/Masters/ReservationModel.cs
@@ -27,6 +27,8 @@ namespace HIMS.API.Models.Masters
         public bool? IsCancelled { get; set; }
         public long? IsCancelledBy { get; set; }
         public DateTime? IsCancelledDateTime { get; set; }
+        public bool? IsAnaesthetistPaid { get; set; }
+        public bool? IsMaterialReplacement { get; set; }
         public List<OtReservationAttendingDetailModel> TOtReservationAttendingDetails { get; set; }
         public List<TOtReservationSurgeryDetailModel> TOtReservationSurgeryDetails { get; set; }
         public List<ReservationDiagnosisModel> TOtReservationDiagnoses { get; set; }
