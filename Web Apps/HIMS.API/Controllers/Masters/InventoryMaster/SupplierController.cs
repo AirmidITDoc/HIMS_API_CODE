919be1cc094048665f5ca77203ef60652eee3ec0
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-01-19 20:17:13 +0530

    SupplierController

--- Web Apps/HIMS.API/Controllers/Masters/InventoryMaster/SupplierController.cs
+++ Web Apps/HIMS.API/Controllers/Masters/InventoryMaster/SupplierController.cs
@@ -34,7 +34,7 @@ namespace HIMS.API.Controllers.Masters.InventoryMaster
             return Ok(SupplierList.ToGridResponse(objGrid, "Supplier List"));
         }
         [HttpGet("{id?}")]
-        [Permission(PageCode = "SupplierMaster", Permission = PagePermission.View)]
+        [Permission]
         public async Task<ApiResponse> Get(int id)
         {
             if (id == 0)
