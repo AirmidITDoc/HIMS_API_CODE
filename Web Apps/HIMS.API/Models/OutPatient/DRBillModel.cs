afe6020d3983c0981cc3e6a1fba6569b1612e488
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-01-20 16:14:28 +0530

    OPDraftBillUpdate

--- Web Apps/HIMS.API/Models/OutPatient/DRBillModel.cs
+++ Web Apps/HIMS.API/Models/OutPatient/DRBillModel.cs
@@ -39,6 +39,12 @@ namespace HIMS.API.Models.OutPatient
         public long? Drno { get; set; }
         public long? ChargesId { get; set; }
     }
+    public class TDrbillDetUpdateModel
+    {
+        public long DrbillDetId { get; set; }
+        public long? Drno { get; set; }
+        public long? ChargesId { get; set; }
+    }
     public class TDraddChargeModel
     {
         public long ChargesId { get; set; }
@@ -96,13 +102,77 @@ namespace HIMS.API.Models.OutPatient
         public long? AddedBy { get; set; }
         public int? CreatedBy { get; set; }
     }
-        public class DraftBillModel
+    public class TDraddChargeUpdateModel
+    {
+        public long ChargesId { get; set; }
+        public DateTime? ChargesDate { get; set; }
+        public string? ChargesTime { get; set; }
+        public byte? OpdIpdType { get; set; }
+        public long? OpdIpdId { get; set; }
+        public long? UnitId { get; set; }
+        public long? ServiceId { get; set; }
+        public long? ClassId { get; set; }
+        public long? TariffId { get; set; }
+        public double? Price { get; set; }
+        public double? Qty { get; set; }
+        public double? TotalAmt { get; set; }
+        public double? ConcessionPercentage { get; set; }
+        public decimal? ConcessionAmount { get; set; }
+        public decimal? NetAmount { get; set; }
+        public long? DoctorId { get; set; }
+        public string? DoctorName { get; set; }
+        public double? DocPercentage { get; set; }
+        public double? DocAmt { get; set; }
+        public double? HospitalAmt { get; set; }
+        public decimal? RefundAmount { get; set; }
+        public long? IsPathology { get; set; }
+        public long? IsRadiology { get; set; }
+        public long? IsDoctorShareGenerated { get; set; }
+        public byte? IsInterimBillFlag { get; set; }
+        public long? IsPackage { get; set; }
+        public long? PackageId { get; set; }
+        public long? PackageMainChargeId { get; set; }
+        public long? IsSelfOrCompanyService { get; set; }
+        public decimal? CPrice { get; set; }
+        public float? CQty { get; set; }
+        public decimal? CTotalAmount { get; set; }
+        public bool? IsComServ { get; set; }
+        public bool? IsPrintCompSer { get; set; }
+        public decimal? ChPrice { get; set; }
+        public float? ChQty { get; set; }
+        public decimal? ChTotalAmount { get; set; }
+        public bool? IsBillableCharity { get; set; }
+        public long? SalesId { get; set; }
+        public bool? IsGenerated { get; set; }
+        public bool? IsApprovedByCamp { get; set; }
+        public int? WardId { get; set; }
+        public int? BedId { get; set; }
+        public string? ServiceCode { get; set; }
+        public string? ServiceName { get; set; }
+        public string? CompanyServiceName { get; set; }
+        public bool? IsInclusionExclusion { get; set; }
+        public int? IsHospMrk { get; set; }
+        public long? BillNo { get; set; }
+        public bool? IsCancelled { get; set; }
+        public long? IsCancelledBy { get; set; }
+        public DateTime? IsCancelledDate { get; set; }
+        public long? AddedBy { get; set; }
+        public int? ModifiedBy { get; set; }
+    }
+    public class DraftBillModel
         {
             public DRBillModel DRBill { get; set; }
             public List<TDrbillDetModel> TDrbillDet { get; set; }
             public List<TDraddChargeModel> TDraddCharge { get; set; }
 
-
         }
+    public class DraftBillUpdateModel
+    {
+        public DRBillModel DRBill { get; set; }
+        public List<TDrbillDetUpdateModel> TDrbillDet { get; set; }
+        public List<TDraddChargeUpdateModel> TDraddCharge { get; set; }
+
+
     }
+}
 
