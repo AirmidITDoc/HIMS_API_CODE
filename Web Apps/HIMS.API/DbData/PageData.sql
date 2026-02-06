0ab69e002411620f3f3314f89572650f10f8d457
Vimal Vataliya <elaunch.vimal@gmail.com>
2026-02-06 21:59:49 +0530

    add menu in sql

--- Web Apps/HIMS.API/DbData/PageData.sql
+++ Web Apps/HIMS.API/DbData/PageData.sql
@@ -7,3 +7,17 @@ SELECT
 * FROM MenuMaster WHERE ISNULL(PermissionCode,'')<>''
 
 -- for generate script for menu table
+
+
+
+------------------- Gastrology----------------------------
+
+IF NOT EXISTS(SELECT 1 FROM MenuMaster WHERE PermissionCode='GastrologyCasePaper')
+	INSERT INTO MenuMaster(UpId,LinkName,Icon,LinkAction,SortOrder,isActive,IsDisplay,PermissionCode,TableNames)
+	values(2,'Gastrology Case Paper','apps','#',99,1,0,'GastrologyCasePaper',NULL)
+IF NOT EXISTS(SELECT 1 FROM MenuMaster WHERE PermissionCode='CheckIn')
+	INSERT INTO MenuMaster(UpId,LinkName,Icon,LinkAction,SortOrder,isActive,IsDisplay,PermissionCode,TableNames)
+	values(2,'Check In','apps','#',99,1,0,'CheckIn',NULL)
+IF NOT EXISTS(SELECT 1 FROM MenuMaster WHERE PermissionCode='CheckOut')
+	INSERT INTO MenuMaster(UpId,LinkName,Icon,LinkAction,SortOrder,isActive,IsDisplay,PermissionCode,TableNames)
+	values(2,'Check Out','apps','#',99,1,0,'CheckOut',NULL)
