9218fff396c055c6237f88abbbd33899a00f047d
uday palli <udaygopalpalli@gmail.com>
2026-02-07 16:20:38 +0530

    OPRequestList

--- Libraries/HIMS.Data/DTO/OPPatient/OPRequestListDto.cs
+++ Libraries/HIMS.Data/DTO/OPPatient/OPRequestListDto.cs
@@ -6,6 +6,7 @@
         public long OP_IP_ID { get; set; }
         public long? ServiceId { get; set; }
         public string? ServiceName { get; set; }
-
+        public long? IsPathology { get; set; }
+        public long? IsRadiology { get; set; }
     }
 }
