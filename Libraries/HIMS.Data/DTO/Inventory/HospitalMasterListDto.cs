4f6c197ddfdca874318804d1c55e61390e9cea6d
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2026-02-09 12:53:46 +0530

    Hospital Master added column

--- Libraries/HIMS.Data/DTO/Inventory/HospitalMasterListDto.cs
+++ Libraries/HIMS.Data/DTO/Inventory/HospitalMasterListDto.cs
@@ -5,6 +5,7 @@
         public long HospitalId { get; set; }
         public string? HospitalHeaderLine { get; set; }
         public string? HospitalName { get; set; }
+        public string? HospitalShortName { get; set; }
         public string? HospitalAddress { get; set; }
         public string? City { get; set; }
         public string? Pin { get; set; }
--- Libraries/HIMS.Data/Models/HIMSDbContext.cs
+++ Libraries/HIMS.Data/Models/HIMSDbContext.cs
@@ -7,14 +7,14 @@ namespace HIMS.Data.Models
 {
     public partial class HIMSDbContext : DbContext
     {
-        ////public HIMSDbContext()
-        ////{
-        ////}
+        //public HIMSDbContext()
+        //{
+        //}
 
-        ////public HIMSDbContext(DbContextOptions<HIMSDbContext> options)
-        ////    : base(options)
-        ////{
-        ////}
+        //public HIMSDbContext(DbContextOptions<HIMSDbContext> options)
+        //    : base(options)
+        //{
+        //}
 
         public virtual DbSet<AddCharge> AddCharges { get; set; } = null!;
         public virtual DbSet<Admission> Admissions { get; set; } = null!;
@@ -613,7 +613,7 @@ namespace HIMS.Data.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=192.168.2.200;Initial Catalog=SSWeb_AIRMID_API;Persist Security Info=True;User ID=DEV001;Password=DEV001;MultipleActiveResultSets=True;Max Pool Size=5000;");
+                optionsBuilder.UseSqlServer("Data Source=192.168.2.200;Initial Catalog=SSWEB_AIRMID_API;Persist Security Info=True;User ID=DEV001;Password=DEV001;MultipleActiveResultSets=True;Max Pool Size=5000;");
             }
         }
 
@@ -2962,6 +2962,8 @@ namespace HIMS.Data.Models
                 entity.Property(e => e.HospitalHeaderLine).HasMaxLength(50);
 
                 entity.Property(e => e.HospitalName).HasMaxLength(100);
+
+                entity.Property(e => e.HospitalShortName).HasMaxLength(100);
 
                 entity.Property(e => e.IpdAdvanceCounterId).HasColumnName("IPD_Advance_CounterId");
 
