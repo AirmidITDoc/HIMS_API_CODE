e4017dc6898c2ea6fd8e0fb3b38cdc9d4c0c3290
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2026-02-11 17:09:16 +0530

    Consent master

--- Libraries/HIMS.Data/DTO/OTManagement/ConsentMasterDto.cs
+++ Libraries/HIMS.Data/DTO/OTManagement/ConsentMasterDto.cs
@@ -13,7 +13,7 @@ namespace HIMS.Data.DTO.OTManagement
         public string? ConsentType { get; set; }
         public string? ConsentName { get; set; }
         public string? ConsentDesc { get; set; }
-        public int? IsActive { get; set; }
+        public bool? IsActive { get; set; }
         public int? CreatedBy { get; set; }
         public string? Value { get; set; }
         public string? DepartmentName { get; set; }
