1808799738ebe0b864f02b6a744c5d6a90c13a26
uday palli <udaygopalpalli@gmail.com>
2026-02-28 10:34:51 +0530

    LabRefundApprovedList new list api created

---
+++ Libraries/HIMS.Data/DTO/Pathology/LabRefundApprovedListDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.Pathology
+{
+    public class LabRefundApprovedListDto
+    {
+        public long RefundId { get; set; }
+        public DateTime? RefundDate { get; set; }
+        public DateTime? RefundTime { get; set; }
+        public string? RefundNo { get; set; }
+        public long BillNo { get; set; }
+
+        public string? PbillNo { get; set; }
+        public long? UnitId { get; set; }
+        public long OpdIpdType { get; set; }
+        public decimal? RefundAmount { get; set; }
+        public string? Remark { get; set; }
+        public bool IsApproval { get; set; }
+        public long ApprovedBy { get; set; }
+        public DateTime ApprovalDatetime { get; set; }
+        public string? Comment { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? LabRequestNo { get; set; }
+
+        public string? CompanyName { get; set; }
+    }
+}
