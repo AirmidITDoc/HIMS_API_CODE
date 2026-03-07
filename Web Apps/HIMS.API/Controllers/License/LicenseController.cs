6f9cddff487e2b4d3691416966dd3278ee9e1282
Vimal Vataliya <elaunch.vimal@gmail.com>
2026-03-07 10:08:34 +0530

    Add apis for regenerate and download license

--- Web Apps/HIMS.API/Controllers/License/LicenseController.cs
+++ Web Apps/HIMS.API/Controllers/License/LicenseController.cs
@@ -1,11 +1,17 @@
 ﻿using Asp.Versioning;
 using DocumentFormat.OpenXml.Vml;
+using DocumentFormat.OpenXml.Wordprocessing;
 using HIMS.Api.Models.Common;
+using HIMS.API.Models;
+using HIMS.API.Utility;
 using HIMS.Data.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 
 namespace HIMS.API.Controllers.License
 {
@@ -50,11 +56,12 @@ namespace HIMS.API.Controllers.License
             return new ApiResponse() { Data = licenseRequests, Message = "License list", StatusCode = 200, StatusText = "Ok" };
         }
         [HttpPost]
-        public ApiResponse Post([FromBody]LicenseRequest obj)
+        public ApiResponse Post([FromBody] LicenseRequest obj)
         {
             string validationMsg = ValidateData(obj.Id, obj.Code, obj.Mac);
             if (validationMsg == "Ok")
             {
+                GenerateLicense(obj);
                 SqlParameter[] para = new SqlParameter[] {
                 new(){ ParameterName="@Id",Value=obj.Id,  SqlDbType=SqlDbType.BigInt },
                 new(){ ParameterName="@HospitalName",Value=obj.HospitalName,  SqlDbType=SqlDbType.NVarChar,Size=250 },
@@ -99,6 +106,107 @@ namespace HIMS.API.Controllers.License
             }
             else
                 return new ApiResponse() { Data = obj, Message = validationMsg, StatusCode = 400, StatusText = "Bad Request" };
+        }
+        [HttpPost]
+        [Route("generate-new-license")]
+        public ApiResponse NewLicense([FromBody] int Id)
+        {
+            LicenseRequest license = GetLicenseData(Id);
+
+            GenerateLicense(license);
+            SqlParameter[] para = new SqlParameter[] {
+                new(){ ParameterName="@Id",Value=Id,  SqlDbType=SqlDbType.BigInt },
+                new(){ ParameterName="@PublicKey",Value=license.PublicKey,  SqlDbType=SqlDbType.NVarChar,Size=-1 },
+                new(){ ParameterName="@PrivateKey",Value=license.PrivateKey,  SqlDbType=SqlDbType.NVarChar,Size=-1 },
+                new(){ ParameterName="@License",Value=license.License,  SqlDbType=SqlDbType.NVarChar,Size=-1 },
+               };
+            using SqlConnection con1 = new("server=192.168.2.200;database=AirmidTech_ProductLicense;uid=DEV001;password=DEV001;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+            using SqlCommand cmd1 = new(@"UPDATE HospitalMaster SET PublicKey=@PublicKey,PrivateKey=@PrivateKey,License=@License WHERE Id=@Id", con1);
+            con1.Open();
+            cmd1.Parameters.AddRange(para);
+            cmd1.ExecuteNonQuery();
+
+            return new ApiResponse() { Data = license, Message = "License request received", StatusCode = 200, StatusText = "Ok" };
+        }
+        [HttpPost("download-license")]
+        public IActionResult GenerateLicense([FromBody] int Id)
+        {
+            LicenseRequest license = GetLicenseData(Id);
+            var bytes = Encoding.UTF8.GetBytes(license.License);
+            return File(bytes, "application/octet-stream", "license.lic");
+        }
+        [NonAction]
+        public LicenseRequest GetLicenseData(int Id)
+        {
+            DataTable dt = new();
+            SqlParameter[] para = new SqlParameter[] {
+                new(){ ParameterName="@Id",Value=Id,  SqlDbType=SqlDbType.BigInt },
+               };
+
+            using SqlConnection con = new("server=192.168.2.200;database=AirmidTech_ProductLicense;uid=DEV001;password=DEV001;MultipleActiveResultSets=True;TrustServerCertificate=True;");
+            using SqlCommand cmd = new("SELECT * FROM HospitalMaster WHERE IsDeleted=0 AND Id=@Id", con);
+            using SqlDataAdapter da = new(cmd);
+            cmd.Parameters.AddRange(para);
+            con.Open();
+            da.Fill(dt);
+            if (dt.Rows.Count == 0)
+                return new LicenseRequest();
+            DataRow dr = dt.Rows[0];
+            return new()
+            {
+                Id = Convert.ToInt32(dr["Id"]),
+                HospitalName = dr["HospitalName"].ToString() ?? string.Empty,
+                Address = dr["Address"].ToString() ?? string.Empty,
+                City = dr["City"].ToString() ?? string.Empty,
+                State = dr["State"].ToString() ?? string.Empty,
+                Country = dr["Country"].ToString() ?? string.Empty,
+                Code = dr["Code"].ToString() ?? string.Empty,
+                PublicKey = dr["PublicKey"].ToString() ?? string.Empty,
+                PrivateKey = dr["PrivateKey"].ToString() ?? string.Empty,
+                License = dr["License"].ToString() ?? string.Empty,
+                Gst = dr["Gst"].ToString() ?? string.Empty,
+                ContactPersonName = dr["ContactPersonName"].ToString() ?? string.Empty,
+                ContactPersonMobile = dr["ContactPersonMobile"].ToString() ?? string.Empty,
+                ExpDate = Convert.ToDateTime(dr["ExpDate"]),
+                MachineName = dr["MachineName"].ToString() ?? string.Empty,
+                Mac = dr["Mac"].ToString() ?? string.Empty
+            };
+        }
+        [NonAction]
+        public void GenerateLicense(LicenseRequest obj)
+        {
+            using var rsa = RSA.Create(2048);
+
+            obj.PrivateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+            obj.PublicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+
+
+            var license = new LicenseModel
+            {
+                Customer = obj.HospitalName,
+                MachineHash = MachineFingerprint.Generate(),
+                ExpiryDate = obj.ExpDate
+            };
+
+            var json = JsonSerializer.Serialize(license);
+
+            using var rsa1 = RSA.Create();
+            rsa1.ImportRSAPrivateKey(Convert.FromBase64String(obj.PrivateKey), out _);
+
+            var signature = rsa1.SignData(
+                Encoding.UTF8.GetBytes(json),
+                HashAlgorithmName.SHA256,
+                RSASignaturePadding.Pkcs1);
+
+            var licenseFile = new
+            {
+                Data = license,
+                Signature = Convert.ToBase64String(signature)
+            };
+
+            //File.WriteAllText("license.lic",
+            //    JsonSerializer.Serialize(licenseFile, new JsonSerializerOptions { WriteIndented = true }));
+            obj.License = JsonSerializer.Serialize(licenseFile, new JsonSerializerOptions { WriteIndented = true });
         }
         [NonAction]
         public static string ValidateData(int Id, string Code, string Mac)
