97e3bbd1de677239277c1e0b591428c79471301e
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2026-03-12 19:33:24 +0530

    InsertLabRequest

--- Libraries/HIMS.Services/Common/IPBillService.cs
+++ Libraries/HIMS.Services/Common/IPBillService.cs
@@ -871,14 +871,16 @@ namespace HIMS.Services.Common
         public virtual void InsertLabRequest(AddCharge ObjaddCharge, int UserId, string UserName, long traiffId, long ReqDetId)
         {
             DatabaseHelper odal = new();
-            string[] AEntity = {  "OpdIpdId","ClassID",  "ServiceId", "TraiffId", "ReqDetId", "UserId", "ChargesDate", "DoctorId"};
+            string[] AEntity = {  "ChargesId","OpdIpdType",  "Price", "Qty", "TotalAmt", "ConcessionPercentage", "ConcessionAmount", "NetAmount",
+                "DocPercentage", "DocAmt", "HospitalAmt", "IsGenerated", "AddedBy", "IsCancelled","IsCancelledDate", "IsPathology", "IsRadiology", "IsPackage", "PackageMainChargeID",
+                "IsSelfOrCompanyService", "PackageId", "ChargesTime", "IsDoctorShareGenerated", "IsInterimBillFlag", "PackageMainChargeId", "RefundAmount", "CPrice", "CQty", "CTotalAmount",
+                "IsComServ", "IsPrintCompSer", "ServiceName", "ChPrice","ChQty","ChTotalAmount","IsBillableCharity","SalesId","IsHospMrk","BillNoNavigation","BillNo","IsCancelledBy","UnitId","TariffId",
+                "DoctorName","ServiceCode","CompanyServiceName","IsInclusionExclusion","WardId","BedId","IsApprovedByCamp","CreatedBy","CreatedDate","ModifiedBy","ModifiedDate","IsOtherService"};
             var entity = ObjaddCharge.ToDictionary();
-
-
-            foreach (var rProperty in entity.Keys.ToList())
+            foreach (var rProperty in AEntity)
             {
-                if (!AEntity.Contains(rProperty))
-                    entity.Remove(rProperty);
+
+                entity.Remove(rProperty);
             }
             
             // Add TraiffId manually to dictionary
