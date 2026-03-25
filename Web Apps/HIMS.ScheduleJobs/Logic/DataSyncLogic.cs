1eddf0c5fcca275866b0a1fd1ea1190fa146fdda
uday palli <udaygopalpalli@gmail.com>
2026-03-25 15:04:42 +0530

    DataSync

---
+++ Web Apps/HIMS.ScheduleJobs/Logic/DataSyncLogic.cs
@@ -0,0 +1,27 @@
+﻿using HIMS.ScheduleJobs.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.ScheduleJobs.Logic
+{
+    public class DataSyncLogic
+    {
+        public DataSyncLogic() { }
+
+        public static async Task RunDataSync()
+        {
+            try
+            {
+                await DatabaseHelper.IUD("EXEC ps_GET_DataSync", Array.Empty<SqlParameter>());
+            }
+            catch (Exception ex)
+            {
+                Common.WriteToFile("Error on " + DateTime.Now.ToString("dd/MM/yyyy") + "=>" + ex.Message);
+            }
+        }
+    }
+}
