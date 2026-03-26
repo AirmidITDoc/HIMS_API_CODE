2ec7bee73e1409fa6742aded5375b3aa7f3ed36b
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-03-26 17:53:18 +0530

    CashlessPatientWiseSummaryList/CashlessCountSummaryList/CashlessCompanyWiseSummaryList

---
+++ Libraries/HIMS.Data/DTO/DashBoard/CashlessPatientWiseSummaryDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.DashBoard
+{
+    public  class CashlessPatientWiseSummaryDto
+    {
+        public DateTime? BillDate { get; set; }
+        public string? VisitCompanyName { get; set; }
+        public DateTime? VisitDate { get; set; }
+        public decimal? BillAmount { get; set; }
+        public double? DiscAmount { get; set; }
+        public decimal? NetBillAmount { get; set; }
+        public decimal? PaidAmount { get; set; }
+        public decimal? BalAmount { get; set; }
+        public long? GovtCompanyId { get; set; }
+        public string? FirstCompanyName { get; set; }
+        public decimal? GovtApprovedAmt { get; set; }
+        public string? GovtRefNo { get; set; }
+        public long? CompanyApprovedId { get; set; }
+        public string? SecondCompanyName { get; set; }
+        public decimal? CompanyApprovedAmt { get; set; }
+        public string? CompRefNo { get; set; }
+    }
+    public class CashlessCountSummaryDto
+    {
+        public double Count { get; set; }
+    }
+    public class CashlessCompanyWiseSummaryDto
+    {
+        public string? CompanyName { get; set; }
+        public double PatientCount { get; set; }
+        public double DraftBill { get; set; }
+        public double FinalAmount { get; set; }
+        public double PharmacyAmount { get; set; }
+
+    }
+
+}
