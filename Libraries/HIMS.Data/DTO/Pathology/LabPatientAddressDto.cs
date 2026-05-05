9003ff22a4af3a1d6686577114a159bdfd62eab8
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-05-05 12:37:57 +0530

    LabPatientAddressList

---
+++ Libraries/HIMS.Data/DTO/Pathology/LabPatientAddressDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Data.DTO.Pathology
+{
+    public  class LabPatientAddressDto
+    {
+        public long AddressId { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? Address { get; set; }
+        public long? CityId { get; set; }
+        public long? StateId { get; set; }
+        public long? CountryId { get; set; }
+        public long? UnitId { get; set; }
+        public bool? IsActive { get; set; }
+        public long? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public long? ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ Libraries/HIMS.Services/Pathlogy/ILabPatientAddresService.cs
@@ -0,0 +1,16 @@
+﻿using HIMS.Core.Domain.Grid;
+using HIMS.Data.DTO.Pathology;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Services.Pathlogy
+{
+    public partial interface ILabPatientAddresService
+    {
+        Task<IPagedList<LabPatientAddressDto>> GetListAsync(GridRequestModel objGrid);
+
+    }
+}
