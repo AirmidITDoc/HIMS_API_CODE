e9d3d381b444bd7a0d6c7bb127cba3587c363990
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-05-12 17:49:39 +0530

    DoctorPaysummarydetailListDto

--- Libraries/HIMS.Data/DTO/Administration/DoctorPaysummarydetailListDto.cs
+++ Libraries/HIMS.Data/DTO/Administration/DoctorPaysummarydetailListDto.cs
@@ -4,31 +4,20 @@
     {
 
         public string? AddChargeDrName { get; set; }
-        public long PBillNo { get; set; }
-        public long BillNo { get; set; }
+        public long? PBillNo { get; set; }
         public long? ChargesId { get; set; }
         public string? ServiceName { get; set; }
-
         public decimal? NetAmount { get; set; }
-
-
         public double? DocAmt { get; set; }
         public decimal? HospitalAmt { get; set; }
-
-
         public decimal? RefundAmount { get; set; }
-
-
-        //public long? DoctorId { get; set; }
         public string? PatientName { get; set; }
         public string? CompanyName { get; set; }
-
         public string? lbl { get; set; }
+        public string? PayMode { get; set; }
 
-        public long IsDoctorShareGenerated { get; set; }
 
-        //public Boolean? IsBillShrHold { get; set; }
-
+       
 
 
 
