654f5fa35c3ea1bf3b1fe5ad2102234158e0dfe6
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-05-20 12:23:33 +0530

    IPPrescriptiononDischargeListDto

--- Libraries/HIMS.Data/DTO/IPPatient/IPPrescriptiononDischargeListDto.cs
+++ Libraries/HIMS.Data/DTO/IPPatient/IPPrescriptiononDischargeListDto.cs
@@ -3,31 +3,16 @@
     public class IPPrescriptiononDischargeListDto
     {
         public long? OPD_IPD_ID { get; set; }
-        //public byte? OpdIpdType { get; set; }
-        //public DateTime? Date { get; set; }
-        //public string? Ptime { get; set; }
-        //public long? ClassId { get; set; }
-        //public long? GenericId { get; set; }
         public long? ItemID { get; set; }
         public long? DoseId { get; set; }
         public long? Days { get; set; }
-        //public long? InstructionId { get; set; }
-        //public double? QtyPerDay { get; set; }
-        //public double? TotalQty { get; set; }
         public string? Instruction { get; set; }
-        //public string? Remark { get; set; }
-        //public bool? IsEnglishOrIsMarathi { get; set; }
-        //public long? StoreId { get; set; }
-        //public int? CreatedBy { get; set; }
-
         public string? ItemName { get; set; }
         public string? DoseNameInEnglish { get; set; }
-
         public string? DoseName { get; set; }
         public string? ItemGenericName { get; set; }
-
-
-
+        public double? QtyPerDay { get; set; }
+        public double? TotalQty { get; set; }
 
 
     }
