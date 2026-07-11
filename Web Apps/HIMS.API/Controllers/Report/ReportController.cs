a2d885fbba09deb736f9b1af1e983ff2e6ae1ecf
uday palli <udaygopalpalli@gmail.com>
2026-07-11 13:06:49 +0530

    Trust Membership html change and Report Controller Context id Enabled

--- Web Apps/HIMS.API/Controllers/Report/ReportController.cs
+++ Web Apps/HIMS.API/Controllers/Report/ReportController.cs
@@ -610,8 +610,8 @@ namespace HIMS.API.Controllers.Report
                     break;
             }
             // PLEASE COMMENT THE SECOUND UNIITID DECLARATION AND UNCOMMENT THE FIRST ONE WHILE CHECKING FROM SWAGGER AND BEFORE PUSHING CODE UNDO THE CHANGES
-            long UnitId = 1;
-            //long UnitId = Context.UnitId;
+           // long UnitId = 1;
+            long UnitId = Context.UnitId;
             model.BaseUrl = AppSettings.Settings.BaseUrl;
             model.StorageBaseUrl = AppSettings.Settings.StorageBaseUrl;
             var byteFile = await _reportService.GetReportSetByProc(model, AppSettings.Settings.PdfFontPath, UnitId);
@@ -625,10 +625,10 @@ namespace HIMS.API.Controllers.Report
             {
 
                 // PLEASE COMMENT THE SECOUND UNIITID DECLARATION AND UNCOMMENT THE FIRST ONE WHILE CHECKING FROM SWAGGER AND BEFORE PUSHING CODE UNDO THE CHANGES
-                long UnitId = 1;
-                long StoreId = 2;
-                //long UnitId = Context.UnitId;
-                //long StoreId = Context.StoreId;
+               // long UnitId = 1;
+                //long StoreId = 2;
+                long UnitId = Context.UnitId;
+                long StoreId = Context.StoreId;
 
                 model.BaseUrl = AppSettings.Settings.BaseUrl;
                 model.StorageBaseUrl = AppSettings.Settings.StorageBaseUrl;
