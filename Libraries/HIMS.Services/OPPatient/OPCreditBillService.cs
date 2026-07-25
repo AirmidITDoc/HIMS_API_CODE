f64225d2520630543c496c391d86588efbc77979
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-07-25 19:07:00 +0530

    OPCreditBillingInsert  API IsOtherService  Column Not Pass in Service File Resolved

--- Libraries/HIMS.Services/OPPatient/OPCreditBillService.cs
+++ Libraries/HIMS.Services/OPPatient/OPCreditBillService.cs
@@ -258,7 +258,7 @@ namespace HIMS.Services.OPPatient
                         {
                             foreach (var item in ObjaddCharge)
                             {
-                                string[] AEntity = { "ChargesId", "ChargesDate", "OpdIpdType", "OpdIpdId", "ServiceId", "Price", "Qty", "UnitId", "TotalAmt", "ConcessionPercentage", "ConcessionAmount", "NetAmount", "DoctorId", "DoctorName", "DocPercentage", "DocAmt", "HospitalAmt", "RefundAmount", "IsGenerated", "IsComServ", "IsPrintCompSer", "AddedBy", "IsCancelled", "IsCancelledBy", "IsCancelledDate", "IsPathology", "IsRadiology", "IsPackage", "WardId", "BedId", "ServiceCode", "ServiceName", "CompanyServiceName", "IsInclusionExclusion", "IsHospMrk", "PackageMainChargeID", "IsSelfOrCompanyService", "PackageId", "ChargesTime", "ClassId", "TariffId", "BillNo", "CreatedBy" };
+                                string[] AEntity = { "ChargesId", "ChargesDate", "OpdIpdType", "OpdIpdId", "ServiceId", "Price", "Qty", "UnitId", "TotalAmt", "ConcessionPercentage", "ConcessionAmount", "NetAmount", "DoctorId", "DoctorName", "DocPercentage", "DocAmt", "HospitalAmt", "RefundAmount", "IsGenerated", "IsComServ", "IsPrintCompSer", "AddedBy", "IsCancelled", "IsCancelledBy", "IsCancelledDate", "IsPathology", "IsRadiology", "IsPackage", "WardId", "BedId", "ServiceCode", "ServiceName", "CompanyServiceName", "IsInclusionExclusion", "IsHospMrk", "PackageMainChargeID", "IsSelfOrCompanyService", "PackageId", "ChargesTime", "ClassId", "TariffId", "BillNo", "CreatedBy", "IsOtherService" };
                                 var Packagescharge = item.ToDictionary();
 
                                 foreach (var rProperty in Packagescharge.Keys.ToList())
