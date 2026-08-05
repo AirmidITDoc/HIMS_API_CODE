9e724ee899ded1e5a61f078fba468289b9e7d2e5
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2026-08-05 21:17:27 +0530

    updated New Document manageent table

---
+++ Libraries/HIMS.Data/Models/DocumentAdmission.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HIMS.Data.Models
+{
+    public partial class DocumentAdmission
+    {
+        public DocumentAdmission()
+        {
+            DocumentFiles = new HashSet<DocumentFile>();
+        }
+
+        public long Id { get; set; }
+        public long AdmissionId { get; set; }
+        public long RegId { get; set; }
+        public bool IsActive { get; set; }
+        public long IsDeleted { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string DocNo { get; set; } = null!;
+
+        public virtual Admission Admission { get; set; } = null!;
+        public virtual Registration Reg { get; set; } = null!;
+        public virtual ICollection<DocumentFile> DocumentFiles { get; set; }
+    }
+}
