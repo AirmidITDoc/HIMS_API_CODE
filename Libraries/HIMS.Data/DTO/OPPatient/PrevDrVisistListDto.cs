7bba1cccfa16bbaa59313ec9df39485bec79bf63
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2026-08-06 20:17:11 +0530

    Added OPD no in Visit Pop up list

--- Libraries/HIMS.Data/DTO/OPPatient/PrevDrVisistListDto.cs
+++ Libraries/HIMS.Data/DTO/OPPatient/PrevDrVisistListDto.cs
@@ -8,12 +8,10 @@
         public long? DepartmentId { get; set; }
         public long? ConsultantDocId { get; set; }
         public DateTime VisitDate { get; set; }
-
         public DateTime RegDate { get; set; }
-
         public long CampId { get; set; }
-
         public long VisitId { get; set; }
         public long? LabPatientId { get; set; }
+        public string OPDNo { get; set; }
     }
 }
