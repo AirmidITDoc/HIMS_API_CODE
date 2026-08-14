6bdf77154c7f625b897a2ac617d0194630d4c75f
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2026-08-14 12:28:08 +0530

    Login Cashounter list added 2 columns

--- Libraries/HIMS.Data/DTO/OPPatient/ServiceMasterDto.cs
+++ Libraries/HIMS.Data/DTO/OPPatient/ServiceMasterDto.cs
@@ -30,6 +30,8 @@ namespace HIMS.Data.DTO.OPPatient
         public long CashCounterId { get; set; }
         public string? CashCounterName { get; set; }
         public bool IsDefault { get; set; }
+        public string? CashCounterType { get; set; }
+        public string? BillNo { get; set; }
 
     }
 }
