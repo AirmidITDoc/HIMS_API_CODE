2f70876911e0de5bfa663a92d4289f2b958beafb
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-08-25 15:29:56 +0530

    TMedicolegalCertificate Insert Update List

---
+++ Web Apps/HIMS.API/Models/MRD/MedicolegalCertificateModel.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using HIMS.API.Models.Masters;
+
+namespace HIMS.API.Models.MRD
+{
+    public class MedicolegalCertificateModel
+    {
+        public long DocId { get; set; }
+        public DateTime? Mlcdate { get; set; }
+        public string? Mlctime { get; set; }
+        public string? CertificateNo { get; set; }
+        public long? OpIpId { get; set; }
+        public byte? OpIpType { get; set; }
+        public DateTime? AccidentDate { get; set; }
+        public string? AccidentTime { get; set; }
+        public string? DetailsInjuries { get; set; }
+        public string? AgeofInjuries { get; set; }
+        public string? CauseofInjuries { get; set; }
+        public long? TreatingDoctorId { get; set; }
+        public long? TreatingDoctorId1 { get; set; }
+        public long? TreatingDoctorId2 { get; set; }
+    }
+    public class MedicolegalCertificateModelValidator : AbstractValidator<MedicolegalCertificateModel>
+    {
+        public MedicolegalCertificateModelValidator()
+        {
+            RuleFor(x => x.Mlcdate).NotNull().NotEmpty().WithMessage("Mlcdate  is required");
+            RuleFor(x => x.Mlctime).NotNull().NotEmpty().WithMessage("Mlctime  is required");
+            RuleFor(x => x.CertificateNo).NotNull().NotEmpty().WithMessage("CertificateNo  is required");
+            RuleFor(x => x.OpIpId).NotNull().NotEmpty().WithMessage("OpIpId  is required");
+            RuleFor(x => x.AccidentDate).NotNull().NotEmpty().WithMessage("AccidentDate  is required");
+            RuleFor(x => x.AccidentTime).NotNull().NotEmpty().WithMessage("AccidentTime  is required");
+            //RuleFor(x => x.Mlcdate).NotNull().NotEmpty().WithMessage("Mlcdate  is required");
+            //RuleFor(x => x.Mlcdate).NotNull().NotEmpty().WithMessage("Mlcdate  is required");
+            //RuleFor(x => x.Mlcdate).NotNull().NotEmpty().WithMessage("Mlcdate  is required");
+            //RuleFor(x => x.Mlcdate).NotNull().NotEmpty().WithMessage("Mlcdate  is required");
+
+        }
+    }
+}
