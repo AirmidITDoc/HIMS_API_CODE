3bbd91dff74fea8d2a000d726322d99b52bd0e24
Samarth Hatture <samarthhatture7@gmail.com>
2026-09-01 19:58:09 +0530

    DeathCertificateList

--- Libraries/HIMS.Data/DTO/MRD/CertifiCateListDto.cs
+++ Libraries/HIMS.Data/DTO/MRD/CertifiCateListDto.cs
@@ -9,61 +9,33 @@ namespace HIMS.Data.DTO.MRD
     public  class CertifiCateListDto
     {
         public long DocId { get; set; }
-
         public DateTime? MLCDate { get; set; }
-
-        public DateTime? MLCTime { get; set; }
-
+        public string? MLCTime { get; set; }
         public string CertificateNo { get; set; }
-
         public long OP_IP_Id { get; set; }
-
         public string PatientName { get; set; }
-
-        public string RegNo { get; set; }
-
+        public long RegNo { get; set; }
         public string DepartmentName { get; set; }
-
         public DateTime? Accident_Date { get; set; }
-
-        public DateTime? Accident_Time { get; set; }
-
+        public string? Accident_Time { get; set; }
         public string CauseofInjuries { get; set; }
-
         public string Details_Injuries { get; set; }
-
         public string AgeofInjuries { get; set; }
-
         public string TreatingDoctorId { get; set; }
-
         public string TreatingDoctorId1 { get; set; }
-
         public string TreatingDoctorId2 { get; set; }
-
         public string UserName { get; set; }
-
         public string Label { get; set; }
-
         public string PatientType { get; set; }
-
         public string HospitalName { get; set; }
-
         public string AdmittedDoctorName { get; set; }
-
         public string RefDoctorName { get; set; }
-
         public string RoomName { get; set; }
-
         public string BedName { get; set; }
-
-        public string IPDNo { get; set; }
-
+        public long IPDNo { get; set; }
         public string CompanyName { get; set; }
-
         public string TariffName { get; set; }
-
         public int IsCancelled { get; set; }
-
         public int OP_IP_Type { get; set; }
     }
 }
