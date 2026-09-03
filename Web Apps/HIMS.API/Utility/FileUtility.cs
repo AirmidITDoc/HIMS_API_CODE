d0356004a20b8105eaa2fde113ecf69a9012e800
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-09-03 22:26:13 +0530

    File Utility Save Image Old Code

--- Web Apps/HIMS.API/Utility/FileUtility.cs
+++ Web Apps/HIMS.API/Utility/FileUtility.cs
@@ -43,21 +43,6 @@ namespace HIMS.API.Utility
             }
             return contentType;
         }
-        //public string SaveImageFromBase64(string Base64, string Folder)
-        //{
-        //    var DestinationPath = "";
-        //    //_Sales.GetFilePath();
-        //    if (string.IsNullOrWhiteSpace(DestinationPath))
-        //        DestinationPath =AppSettings.Settings.StorageBaseUrl;
-        //    if (!Directory.Exists(DestinationPath))
-        //        Directory.CreateDirectory(DestinationPath);
-        //    if (!Directory.Exists($"{DestinationPath.Trim('\\')}\\{Folder}"))
-        //        Directory.CreateDirectory($"{DestinationPath.Trim('\\')}\\{Folder}");
-        //    string FilePath = Path.Combine(DestinationPath.Trim('\\'), Folder.Trim('\\'));
-        //    string FileName = Guid.NewGuid().ToString() + ".png";
-        //    File.WriteAllBytes(Path.Combine(FilePath, FileName), Convert.FromBase64String(Base64.Replace("data:image/png;base64,", "")));
-        //    return FileName;
-        //}
         public string SaveImageFromBase64(string Base64, string Folder)
         {
             var DestinationPath = "";
@@ -66,15 +51,30 @@ namespace HIMS.API.Utility
                 DestinationPath = AppSettings.Settings.StorageBaseUrl;
             if (!Directory.Exists(DestinationPath))
                 Directory.CreateDirectory(DestinationPath);
-
-            string FilePath = Path.Combine(DestinationPath.TrimEnd('\\', '/'), Folder.Trim('\\', '/'));
-            if (!Directory.Exists(FilePath))
-                Directory.CreateDirectory(FilePath);
-
+            if (!Directory.Exists($"{DestinationPath.Trim('\\')}\\{Folder}"))
+                Directory.CreateDirectory($"{DestinationPath.Trim('\\')}\\{Folder}");
+            string FilePath = Path.Combine(DestinationPath.Trim('\\'), Folder.Trim('\\'));
             string FileName = Guid.NewGuid().ToString() + ".png";
             File.WriteAllBytes(Path.Combine(FilePath, FileName), Convert.FromBase64String(Base64.Replace("data:image/png;base64,", "")));
             return FileName;
         }
+        //public string SaveImageFromBase64(string Base64, string Folder)
+        //{
+        //    var DestinationPath = "";
+        //    //_Sales.GetFilePath();
+        //    if (string.IsNullOrWhiteSpace(DestinationPath))
+        //        DestinationPath = AppSettings.Settings.StorageBaseUrl;
+        //    if (!Directory.Exists(DestinationPath))
+        //        Directory.CreateDirectory(DestinationPath);
+
+        //    string FilePath = Path.Combine(DestinationPath.TrimEnd('\\', '/'), Folder.Trim('\\', '/'));
+        //    if (!Directory.Exists(FilePath))
+        //        Directory.CreateDirectory(FilePath);
+
+        //    string FileName = Guid.NewGuid().ToString() + ".png";
+        //    File.WriteAllBytes(Path.Combine(FilePath, FileName), Convert.FromBase64String(Base64.Replace("data:image/png;base64,", "")));
+        //    return FileName;
+        //}
         public async Task<string> UploadFileAsync(IFormFile file, string Folder)
         {
             var DestinationPath = "";
