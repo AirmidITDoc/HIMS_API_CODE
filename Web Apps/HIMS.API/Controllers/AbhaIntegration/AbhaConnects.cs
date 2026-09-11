7768dd29fba587cdd5944f447ee2d1d6e338fdf8
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2026-09-11 16:40:54 +0530

    Added Abha Integration

---
+++ Web Apps/HIMS.API/Controllers/AbhaIntegration/AbhaConnects.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HIMS.API.Controllers.AbhaIntegration
+{
+    public class AbhaConnects : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
