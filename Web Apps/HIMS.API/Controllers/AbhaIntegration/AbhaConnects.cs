28c6bbd78cbcef36c44896404e9c53d95111ec26
uday palli <udaygopalpalli@gmail.com>
2026-09-12 11:46:03 +0530

    Abha Connect Version Error

--- Web Apps/HIMS.API/Controllers/AbhaIntegration/AbhaConnects.cs
+++ Web Apps/HIMS.API/Controllers/AbhaIntegration/AbhaConnects.cs
@@ -1,11 +1,16 @@
-﻿using HIMS.API.Models.AbhaIntegration;
+﻿using Asp.Versioning;
+using HIMS.API.Models.AbhaIntegration;
 using HIMS.Services.AbhaIntegration;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HIMS.API.Controllers.AbhaIntegration
 {
+    //[ApiController]
+   // [Route("api/[controller]")]
+
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
-    [Route("api/[controller]")]
+    [ApiVersion("1")]
     public class AbhaConnects : ControllerBase
     {
         private readonly IAbhaConnectService _abhaConnectService;
