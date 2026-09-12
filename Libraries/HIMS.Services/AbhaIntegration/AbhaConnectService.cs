d2ed67117692350a55d186b9b611a7c3a7695559
uday palli <udaygopalpalli@gmail.com>
2026-09-12 15:31:08 +0530

    Abha connects

--- Libraries/HIMS.Services/AbhaIntegration/AbhaConnectService.cs
+++ Libraries/HIMS.Services/AbhaIntegration/AbhaConnectService.cs
@@ -26,31 +26,42 @@ namespace HIMS.Services.AbhaIntegration
             var abhaClientSessionUrl = _configuration["ABDMIntegration:ClientSessionUrl"];
             var abhaBaseUrl = _configuration["ABDMIntegration:BaseUrl"];
 
-            var request = new HttpRequestMessage(
-                HttpMethod.Post, $"{abhaClientSessionUrl}"
-            );
+            var request = new HttpRequestMessage(HttpMethod.Post, abhaClientSessionUrl);
 
             request.Headers.Add("accept", "*/*");
             request.Headers.Add("x-org-id", "1");
 
             var json = JsonSerializer.Serialize(model);
-
-            request.Content = new StringContent(json,Encoding.UTF8,"application/json");
+            request.Content = new StringContent(json, Encoding.UTF8, "application/json");
 
             var response = await client.SendAsync(request);
             var result = await response.Content.ReadAsStringAsync();
+
             var jsonObject = JsonNode.Parse(result).AsObject();
             var responseCode = jsonObject["responseCode"]?.GetValue<int>();
+            var responseMessage = jsonObject["responseMessage"]?.GetValue<string>();
 
             if (responseCode != 200)
             {
-                return jsonObject;
+                return new
+                {
+                    ResponseCode = responseCode,
+                    ResponseMessage = responseMessage,
+                    ResponseData = jsonObject["responseData"]
+                };
             }
+
             var transactionId = jsonObject["responseData"]?["transactionId"]?.GetValue<string>();
-            var callbackUrl =$"{abhaBaseUrl}/{transactionId}";
+            var callbackUrl = $"{abhaBaseUrl}/{transactionId}";
 
             jsonObject["responseData"]["callbackUrl"] = callbackUrl;
-            return jsonObject;
+
+            return new
+            {
+                ResponseCode = responseCode,
+                ResponseMessage = responseMessage,
+                ResponseData = jsonObject["responseData"]
+            };
         }
     }
 }
