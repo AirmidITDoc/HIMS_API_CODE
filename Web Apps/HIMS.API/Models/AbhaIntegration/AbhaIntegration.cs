729a8d983c7acc2c51100054ad8ad476b96f680c
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2026-09-12 17:21:46 +0530

    Added AbhaCallbackModel

--- Web Apps/HIMS.API/Models/AbhaIntegration/AbhaIntegration.cs
+++ Web Apps/HIMS.API/Models/AbhaIntegration/AbhaIntegration.cs
@@ -11,4 +11,23 @@
         public string requestType { get; set; }
         public string callbackUrl { get; set; }
     }
+
+    public class AbhaCallbackModel
+    {
+        public object patientId { get; set; }
+        public string patientName { get; set; }
+        public string sbxId { get; set; }
+        public string gender { get; set; }
+        public string mobileNumber { get; set; }
+        public string dob { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string createdDate { get; set; }
+        public int clientId { get; set; }
+        public string abhaNumber { get; set; }
+        public string json { get; set; }
+        public string transactionId { get; set; }
+    }
+
+
 }
