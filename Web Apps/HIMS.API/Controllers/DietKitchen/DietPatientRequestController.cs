a05c1c6d3719b9550a261e6e435ec0fbec9af4a7
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-09-15 17:09:09 +0530

    DietPatientRequestController

---
+++ Web Apps/HIMS.API/Controllers/DietKitchen/DietPatientRequestController.cs
@@ -0,0 +1,11 @@
+﻿using HIMS.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HIMS.API.Controllers.DietKitchen
+{
+    public class DietPatientRequestController : BaseController
+    {
+       
+    }
+
+}
