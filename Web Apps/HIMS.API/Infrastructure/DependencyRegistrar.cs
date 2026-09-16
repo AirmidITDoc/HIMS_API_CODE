227b683d9556f71b346863147fdcbf79f3db662f
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2026-09-16 10:53:19 +0530

    DietPatient Requestioncontroller

--- Web Apps/HIMS.API/Infrastructure/DependencyRegistrar.cs
+++ Web Apps/HIMS.API/Infrastructure/DependencyRegistrar.cs
@@ -12,6 +12,7 @@ using HIMS.Services.Audit;
 using HIMS.Services.Canteen;
 using HIMS.Services.Common;
 using HIMS.Services.Dashboard;
+using HIMS.Services.DietKitchen;
 using HIMS.Services.DoctorPayout;
 using HIMS.Services.DocumentManagement;
 using HIMS.Services.FeedBack;
