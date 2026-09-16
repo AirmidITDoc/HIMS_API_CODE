5e9ec227bba128ceb8b5aaa6612f4a596637bfda
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2026-09-16 14:48:07 +0530

    /api/v{version}/MealTypeMaster, /api/v{version}/FoodCategoryMaster /api/v{version}/DietMenuMaster/Insert

--- Web Apps/HIMS.API/Infrastructure/ApplicationMappingProfile.cs
+++ Web Apps/HIMS.API/Infrastructure/ApplicationMappingProfile.cs
@@ -205,6 +205,10 @@ namespace HIMS.API.Infrastructure
             CreateMap<MTemplateMaster, PathTemplateModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<DoctorMaster, DoctoreMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<DoctorMaster, DoctorModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<MFoodCategoryMaster, FoodCategorymasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<MMealTypeMaster, MealTypeMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+
+
 
             CreateMap<MTermsOfPaymentMaster, TermsOfPaymentModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<TSalesDraftHeader, TSalesDraftsHeaderModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
@@ -492,6 +496,7 @@ namespace HIMS.API.Infrastructure
             CreateMap<MOtSiteDescriptionMaster, SiteDescriptionModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<MConsentMaster, ConsentMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<MSystemConfig, SystemConfigUpdate>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+
 
 
 
@@ -936,6 +941,10 @@ namespace HIMS.API.Infrastructure
             CreateMap<TOtReservationHeader, TOtRequestDiagnosisModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<VisitDetail, FollowupDateUpdateModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
             CreateMap<MIcdDiagnosisMaster, MIcdDiagnosisMasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<MDietMenuMaster, DietmenumasterModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+            CreateMap<MDietMenuDetailMaster, DietmenumasterDetailsModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
+
+
 
 
             CreateMap<TBatchAdjustment, BatchAdjustmentModel>().ReverseMap().IgnoreAllPropertiesWithAnInaccessibleSetter();
