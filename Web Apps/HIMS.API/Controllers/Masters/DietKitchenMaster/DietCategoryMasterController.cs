5dac4b57f244b5806e144d236c506c6af14516d7
shilpa22011999 <156453175+shilpa22011999@users.noreply.github.com>
2026-09-17 16:22:59 +0530

    DietPatientRequestDetailsList Added Column CancelledUser/IsAcceptedUser/DietPatientRequestHeaderList Added Column User Name / MDietCategoryMaster Insert ,Update List,Delete

---
+++ Web Apps/HIMS.API/Controllers/Masters/DietKitchenMaster/DietCategoryMasterController.cs
@@ -0,0 +1,102 @@
+﻿using Asp.Versioning;
+using HIMS.Api.Controllers;
+using HIMS.Api.Models.Common;
+using HIMS.API.Extensions;
+using HIMS.API.Models.Diet;
+using HIMS.Core.Domain.Grid;
+using HIMS.Core.Infrastructure;
+using HIMS.Data;
+using HIMS.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HIMS.API.Controllers.Masters.DietKitchenMaster
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1")]
+    public class DietCategoryMasterController : BaseController
+    {
+        private readonly IGenericService<MDietCategoryMaster> _repository;
+        public DietCategoryMasterController(IGenericService<MDietCategoryMaster> repository)
+        {
+            _repository = repository;
+        }
+        //List API
+        [HttpPost]
+        [Route("[action]")]
+        [Permission]
+        public async Task<IActionResult> List(GridRequestModel objGrid)
+        {
+            IPagedList<MDietCategoryMaster> DietCategoryMasterList = await _repository.GetAllPagedAsync(objGrid);
+            return Ok(DietCategoryMasterList.ToGridResponse(objGrid, "DietCategoryMaster List"));
+        }
+        //List API Get By Id
+        [HttpGet("{id?}")]
+        //[Permission]
+        public async Task<ApiResponse> Get(int id)
+        {
+            if (id == 0)
+            {
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status400BadRequest, "No data found.");
+            }
+            var data = await _repository.GetById(x => x.DietCategoryId == id);
+            return data.ToSingleResponse<MDietCategoryMaster, DietCategoryMasterModel>("DietCategoryMaster");
+        }
+        //Add API
+        [HttpPost]
+        [Permission]
+        public async Task<ApiResponse> Post(DietCategoryMasterModel obj)
+        {
+            MDietCategoryMaster model = obj.MapTo<MDietCategoryMaster>();
+            model.IsActive = true;
+            if (obj.DietCategoryId == 0)
+            {
+                model.CreatedBy = CurrentUserId;
+                model.CreatedDate = AppTime.Now;
+                model.ModifiedBy = CurrentUserId;
+                model.ModifiedDate = AppTime.Now;
+                await _repository.Add(model, CurrentUserId, CurrentUserName);
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Record  added successfully.");
+        }
+        //Edit API
+        [HttpPut("{id:int}")]
+        [Permission]
+        public async Task<ApiResponse> Edit(DietCategoryMasterModel obj)
+        {
+            MDietCategoryMaster model = obj.MapTo<MDietCategoryMaster>();
+            model.IsActive = true;
+            if (obj.DietCategoryId == 0)
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+            else
+            {
+                model.ModifiedBy = CurrentUserId;
+                model.ModifiedDate = AppTime.Now;
+                await _repository.Update(model, CurrentUserId, CurrentUserName, new string[2] { "CreatedBy", "CreatedDate" });
+            }
+            return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Record  updated successfully.");
+        }
+        //Delete API
+        [HttpDelete]
+        [Permission]
+        public async Task<ApiResponse> Delete(int Id)
+        {
+            MDietCategoryMaster model = await _repository.GetById(x => x.DietCategoryId == Id);
+            if ((model?.DietCategoryId ?? 0) > 0)
+            {
+                model.IsActive = model.IsActive == true ? false : true;
+                model.ModifiedBy = CurrentUserId;
+                model.ModifiedDate = AppTime.Now;
+                await _repository.SoftDelete(model, CurrentUserId, CurrentUserName);
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status200OK, "Record  deleted successfully.");
+            }
+            else
+                return ApiResponseHelper.GenerateResponse(ApiStatusCode.Status500InternalServerError, "Invalid params");
+        }
+
+
+
+    }
+}
