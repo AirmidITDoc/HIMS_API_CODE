db1c25c99096ada4bb1b6eb97bb77c21d1ce38c2
katkarashutosh17@gmail.com <115867281+Ashukatkar@users.noreply.github.com>
2026-09-23 14:20:53 +0530

    TypesOfFoodItemUnits-- dropdown /api/v{version}/DietMenuMaster/DietmenumasterDetailsList /api/v{version}/DietMenuMaster/DietmenumasterList

---
+++ Libraries/HIMS.Services/DietKitchen/DietmenuDetailmasterListDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.Services.DietKitchen
+{
+    public class DietmenuDetailmasterListDto
+    {
+        public long DietMenuId { get; set; }
+        public string DietMenuCode { get; set; } 
+        public string DietMenuName { get; set; } 
+        public string FoodName { get; set; } 
+        public string Name { get; set; } 
+        public string ConstantType { get; set; }
+        public string DietMeValuenuName { get; set; } 
+        public long Quantity { get; set; } 
+
+
+    }
+}
+
